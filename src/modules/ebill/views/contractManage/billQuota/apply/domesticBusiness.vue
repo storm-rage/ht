54d8da9b10f38584c758c72e6234373668887d4d
suyc <1232dev@ht.com>
2022-06-30 19:10:18 +0800

    额度申请

--- src/modules/ebill/views/contractManage/billQuota/apply/domesticBusiness.vue
+++ src/modules/ebill/views/contractManage/billQuota/apply/domesticBusiness.vue
@@ -4,33 +4,30 @@
     <zj-content-block>
       <zj-header title="合同信息"></zj-header>
       <zj-content>
-        <div
-          role="tabpanel"
-          aria-labelledby="el-collapse-head-2884"
-          id="el-collapse-content-2884"
-          class="el-collapse-item__wrap"
-        >
-          <div class="el-collapse-item__content">
-            <div class="infoItem kdjycx">
-              <div class="el-row">
-                <div class="el-col el-col-8">
-                  <label>合同类型：</label><span>国内商业保理合同</span>
-                </div>
-                <div class="el-col el-col-8">
-                  <label>合同编号：</label><span>xxxxxxx</span>
-                </div>
-                <div class="el-col el-col-8">
-                  <label>合同有效期间：</label
-                  ><span> 2022.09.09 至 2023.09.08</span>
-                </div>
-                <div class="el-col el-col-8">
-                  <label>合同状态：</label><span>生效</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <el-form label-width="120px" ref="form" size="mini">
+          <el-row>
+            <el-col :span="8">
+              <el-form-item label="合同类型：">
+                <span>国内商业保理合同</span>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="合同编号：">
+                <span>xxxxxxx</span>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="合同有效期间：">
+                <span>2022.09.09 至 2023.09.08</span>
+              </el-form-item>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-form-item label="合同状态：">
+              <span>生效</span>
+            </el-form-item>
+          </el-row>
+        </el-form>
         <zj-table>
           <zj-table-column field="issueEntName" title="序号" />
           <zj-table-column
