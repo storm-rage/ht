49dd9e6092e8b7a7a458effeb8d09d0d26788b78
郭荣 <guorong01234@163.com>
2022-07-08 11:49:29 +0800

    平台管理页面

---
+++ src/modules/base/views/platformManager/basicInfo/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="basicInfo">
+    <ZjTopHeader>平台基础信息设置</ZjTopHeader>
+
+    <ZjHeader>平台信息</ZjHeader>
+    <div class="formBox">
+      <el-form
+        ref="form"
+        class="mbi-form"
+        :model="formModel"
+        :rules="formRules"
+        :class="editFlag ? '' : 'nmb0'"
+        label-width="180px"
+      >
+        <el-form-item label="平台名称：">
+          <el-input
+            v-model="searchForm.issueEntName"
+            @keyup.enter.native="enterSearch"
+          />
+        </el-form-item>
+        <el-form-item label="平台平台运营企业名称：">
+          <el-input
+            v-model="searchForm.issueEntName"
+            @keyup.enter.native="enterSearch"
+          />
+        </el-form-item>
+      </el-form>
+    </div>
+    <ZjHeader>运营信息</ZjHeader>
+    <h4>邮寄地址</h4>
+    
+    <div class="formBox">
+      <el-form
+        ref="form1"
+        class="mbi-form"
+        :model="formModel"
+        :rules="formRules"
+        :class="editFlag ? '' : 'nmb0'"
+        label-width="180px"
+      >
+        <el-form-item label="收件人：">
+          <el-input
+            v-model="searchForm.issueEntName"
+            @keyup.enter.native="enterSearch"
+          />
+        </el-form-item>
+        <el-form-item label="收件人电话：">
+          <el-input
+            v-model="searchForm.issueEntName"
+            @keyup.enter.native="enterSearch"
+          />
+        </el-form-item>
+        <el-form-item label="收件地址：">
+          <el-input
+            v-model="searchForm.issueEntName"
+            @keyup.enter.native="enterSearch"
+          />
+        </el-form-item>
+        <el-form-item label="客服电话：">
+          <el-input
+            v-model="searchForm.issueEntName"
+            @keyup.enter.native="enterSearch"
+          />
+        </el-form-item>
+      </el-form>
+    </div>
+    <ZjContentFooter>
+      <div class="footerBtnBox">
+        <zj-button type="primary">确认</zj-button>
+        <zj-button>返回</zj-button>
+      </div>
+    </ZjContentFooter>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            searchForm: {}
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+.basicInfo {
+    > h4 {
+        margin-left: 40px;
+        margin-bottom: 10px;
+        font-size: 16px;
+    }
+}
+</style>
---
+++ src/modules/base/views/platformManager/modelAggreement/index.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="modelAggreement">
+    <div class="zj-search-condition">
+      <el-row class="button-row">
+        <zj-button class="reset" icon="el-icon-refresh" @click="resetSearch"
+          >重置</zj-button
+        >
+        <zj-button class="search" icon="el-icon-search" @click="search"
+          >查询</zj-button
+        >
+      </el-row>
+      <el-form ref="searchForm" :model="searchForm">
+        <el-form-item label="协议文本编号：">
+          <el-input
+            v-model="searchForm.issueEntName"
+            @keyup.enter.native="enterSearch"
+          />
+        </el-form-item>
+        <el-form-item label="协议类型：" class="col-center">
+          <el-select
+            v-model="searchForm.isGenerateVoucher"
+            placeholder="请选择"
+            clearable
+            :popper-append-to-body="false"
+          >
+            <el-option value="" label="全部"></el-option>
+            <!-- <el-option
+              v-for="item in dictionary.isGenerateVouchers"
+              :key="item.code"
+              :label="item.desc"
+              :value="item.code"
+            >
+            </el-option> -->
+          </el-select>
+        </el-form-item>
+        <el-form-item label="最近维护日期：" class="col-right">
+          <zj-date-range-picker
+            :startDate.sync="searchForm.expireDateStart"
+            :endDate.sync="searchForm.expireDateEnd"
+          />
+        </el-form-item>
+        <el-form-item label="模板类型：" class="col-center">
+          <el-select
+            v-model="searchForm.isGenerateVoucher"
+            placeholder="请选择"
+            clearable
+            :popper-append-to-body="false"
+          >
+            <el-option value="" label="全部"></el-option>
+            <!-- <el-option
+              v-for="item in dictionary.isGenerateVouchers"
+              :key="item.code"
+              :label="item.desc"
+              :value="item.code"
+            >
+            </el-option> -->
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </div>
+
+    <div class="zj-search-response">
+      <zj-table
+        ref="searchTable"
+        :params="searchForm"
+        :api="zjControl.tableApi"
+      >
+        <zj-table-column field="ebillCode" title="协议类型" />
+        <zj-table-column field="issueEntName" title="协议文本编号" />
+        <zj-table-column field="issueEntName" title="文件名称" />
+        <zj-table-column field="issueEntName" title="附件" />
+        <zj-table-column field="issueEntName" title="模板状态" />
+        <zj-table-column field="issueEntName" title="版本号" />
+        <zj-table-column
+          field="issueEntName"
+          title="最近维护日期"
+          :formatter="date"
+        />
+        <zj-table-column title="操作" fixed="right">
+          <template v-slot="{ row }">
+            <zj-button
+              type="text"
+              @click="toDetail(row)"
+              :api="zjBtn.getEnterprise"
+              >下载</zj-button
+            >
+          </template>
+        </zj-table-column>
+      </zj-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      zjControl: {},
+      searchForm: {},
+      dialogVisible: false,
+      type: "info",
+      tableData: [{ id: 1 }],
+      formModel: {},
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+.modelAggreement {
+}
+</style>
