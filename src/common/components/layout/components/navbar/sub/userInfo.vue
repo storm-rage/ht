a27181a5b5966206cd57c9894bc5f2a149072a3b
suyc <1232dev@ht.com>
2022-07-12 19:59:23 +0800

    #feat:调整菜单和顶部

---
+++ src/common/components/layout/components/navbar/sub/userInfo.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="navbar-user-info">
+    <el-dropdown trigger="click" placement="bottom" class="navbar-right-menu" @command="handleClickDropdown" v-if="userInfo.userName">
+        <span class="menu-dropdown-link">
+         <i class="ht-iconfont hd-touxiang_avatar"></i>&nbsp;{{ userInfo.userName }}<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item command="center">个人中心</el-dropdown-item>
+        <el-dropdown-item command="logout">退   出</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
+</template>
+<script>
+import {mapState} from "vuex";
+
+export default {
+  computed: {
+    ...mapState({
+      userInfo: state => state.user.userInfo
+    })
+  },
+  methods: {
+    handleClickDropdown (name) {
+      if(name==='center') {
+        // todo: 跳转个人中心
+      }else if (name === 'logout') {
+        this.$confirm('您确定要退出吗？','提示',{
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.logout();
+        })
+      }
+    },
+    // 登出
+    logout () {
+      this.$api.login.logout().then(()=>{
+        //清除local存储的信息
+        sessionStorage.clear()
+        localStorage.clear()
+        window.clearVuexAlong()
+        this.$store.commit('resetState')
+        this.$store.dispatch('menu/SET_MENU_ACTIVE','')
+        this.defaultOpenedsArray = []
+        this.$router.replace({name: 'login'})
+      })
+    }
+  },
+}
+</script>
+<style lang="less" scoped>
+.navbar-user-info {
+  font-weight: 400;
+  color: #303133;
+  .navbar-right-menu {
+    .menu-dropdown-link {
+      cursor: pointer;
+    }
+  }
+}
+</style>
--- src/common/components/layout/components/sidebar/Sidebar.vue
+++ src/common/components/layout/components/sidebar/Sidebar.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="siderbar" :class="collapseFlag ? 'min-siderbar' : ''">
-    <div class="logoBox">
-      <div class="user" @click="toHome">
-        <span :class="[
-          collapseFlag ? 'p-c-s' : ''
-        ]">
-          <img src="~@assets/img/layout/homeWorker.png" alt="">
-        </span>
-        <label v-if="!collapseFlag">工作台</label>
-        <i :class="[
-          defaultOpenedsArray.length ? 'el-icon-s-unfold' : 'el-icon-s-fold',
-          collapseFlag ? 'd-none' : ''
-        ]" @click="collapseFlagClick"/>
-      </div>
-    </div>
-    <el-scrollbar class="h-c-100_41 zj-p-t-5 zj-p-b-5 shadowRight" wrapStyle="overflow-x: auto;">
+    <el-scrollbar class="zj-p-b-5 siderbar-scrollbar">
       <el-menu
         ref="zjElMenu"
         :default-active="activeMenu"
@@ -23,7 +9,15 @@
         style="border-right: 0;height: 100%"
         :class="collapseFlag ? 'w50px' : '' "
         :collapse="collapseFlag"
+        background-color="#2D313F"
+        text-color="#fff"
       >
+        <div class="menu-tree">
+          <el-menu-item :class="{'is-active':$route.name === 'home'}"  index="home" @click="toHome">
+            <i class="icon-img" :class="['ht-iconfont hd-shouye_home',{'menu-collapse': collapseFlag}]"/>
+            <span slot="title">平台首页</span>
+          </el-menu-item>
+        </div>
         <menu-tree :menuData="sideMenuTreeList" :collapse="collapseFlag"/>
       </el-menu>
     </el-scrollbar>
@@ -58,35 +52,11 @@ export default {
     toRouter (menu) {
       this.$router.push(menu.url)
     },
-    //跳转至个人信息
-    goUserInfoVue(){
-      this.$router.push({name:'userInfo'})
-      this.$store.dispatch('menu/SET_MENU_ACTIVE','')
-    },
     setDefaultOpenedsArray (isExpandAll=true) {// 设置默认展开
       this.defaultOpenedsArray = [];
     },
-    //初始化顶部栏选中 --- 递归
-    getNavActive(children,name){
-      let key = []
-      if(Array.isArray(children)){
-        for(let i=0; i<children.length; i++){
-          if(children[i].url === name){
-            key.push(true)
-            break
-          }
-          else if(children[i].children){
-            key = [...this.getNavActive(children[i].children,name),...key]
-          }else {
-            key.push(false)
-          }
-        }
-      }
-      return key
-    },
     //菜单折叠
     collapseFlagClick(){
-      // this.$store.dispatch('menu/setCollapseFlag',!this.collapseFlag)
       let idList = this.sideMenuTreeList.map(item => item.id)
       this.$refs.zjElMenu.openedMenus.length ? this.defaultOpenedsArray = [] : this.defaultOpenedsArray = idList
     },
@@ -98,27 +68,11 @@ export default {
         routeName = this.row.parent
         editFlag = true
       }
-      //查找顶部选中
-      for(let i=0; i<this.menuTreeList.length; i++){
-        let newArr = this.getNavActive(this.menuTreeList[i].children,routeName)
-        if(newArr.findIndex(item => item===true) >= 0){
-          this.$store.dispatch('menu/setNavMenuActive', i)
-          let mentT = JSON.parse(JSON.stringify(this.$store.state.menu.menuTreeList))
-          this.sideMenuTreeList = mentT || []
-          if(!editFlag){
-            router.meta.parent ? this.activeMenu = router.meta.parent : this.activeMenu = routeName
-          }else{
-            this.activeMenu = routeName
-          }
-          break
-        }
-      }
       // 加载当前菜单树
       let mentT = JSON.parse(JSON.stringify(this.$store.state.menu.menuTreeList))
       this.sideMenuTreeList = mentT || []
       this.activeMenu = this.menuActive
       //设置菜单栏选中
-      // this.$store.dispatch('tab/addCache',newRou)
       if(!editFlag && router.meta.parent){
         this.$store.dispatch('menu/SET_MENU_ACTIVE',router.meta.parent)
         this.activeMenu = router.meta.parent
@@ -129,14 +83,10 @@ export default {
           this.setDefaultOpenedsArray();
           this.activeMenu = ''
         }else{
-          // this.$store.dispatch('menu/SET_MENU_ACTIVE',routeName)
           this.$store.commit('menu/SET_MENU_ACTIVE',routeName)
           this.$nextTick(() => {
             this.activeMenu = this.menuActive
           })
-          // mounted代码
-          // this.$store.dispatch('menu/SET_MENU_ACTIVE',routeName)
-          // this.activeMenu = routeName
         }
       }
     }
@@ -144,7 +94,6 @@ export default {
   },
   computed: {
     ...mapState({
-      navMenuActive: state => state.menu.navMenuActive, //顶部选中
       collapseFlag: state => state.menu.collapseFlag,
       menuActive: state => state.menu.menuActive, //菜单树选中
       userInfo: state => state.user.userInfo,
@@ -153,18 +102,6 @@ export default {
     })
   },
   watch: {
-    navMenuActive(val){
-      this.sideMenuTreeList = []
-      // this.menuLoading = true
-      if(!this.$store){return}
-
-      let mentT = JSON.parse(JSON.stringify(this.$store.state.menu.menuTreeList))
-      if(!mentT.length){return}
-      // this.menuLoading = false
-      this.$nextTick(() => {
-        this.sideMenuTreeList = mentT || []
-      })
-    },
     menuActive(val){
       this.$store.dispatch('menu/SET_MENU_ACTIVE',val)
       this.activeMenu = val
@@ -184,74 +121,6 @@ export default {
   }
 }
 </script>
-<style lang="less" scoped>
-.logoBox{
-  width: 100%;
-  /*background-color: #33649D;*/
-  background-color: #FFFFFF;
-  border-bottom: 1px solid #eee;
-  .logo{
-    height: 106px;
-    img{
-      width: 96%;
-      margin: 45px auto 0 3%;
-    }
-  }
-  .user{
-    position: relative;
-    height: 40px;
-    text-align: center;
-    line-height: 40px;
-    border-right: 1px solid rgba(0,0,0,0.2);
-    span{
-      font-size: 18px;
-      color: #9df8cc;
-      margin-right: 5px;
-      img {
-        width: 20px;
-        height: 20px;
-        vertical-align: middle;
-      }
-    }
-    label{
-      color: #333333;
-      letter-spacing: 1px;
-    }
-    .d-none{
-      display: none;
-    }
-    .p-c-s{
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      transform: translate(-50%,-50%);
-    }
-    i{
-      cursor: pointer;
-      position: absolute;
-      right: 9%;
-      top: 50%;
-      transform: translateY(-50%);
-      font-size: 18px;
-
-    }
-  }
-}
-</style>
-<style lang="less">
-.el-submenu__icon-arrow{
-  font-weight: bold!important;
-  font-size: 14px!important;
-  margin-top: -6px!important;
-  /*color: burlywood!important;*/
-  color: #BDCFDF !important;
-}
-.el-submenu.is-active.is-opened{
-  .el-submenu__icon-arrow{
-    color: rgba(222, 184, 135, 0.71) !important;
-  }
-}
-</style>
 <style lang="less">
 //收缩
 .min-siderbar{
@@ -276,11 +145,6 @@ export default {
       padding-left: 0!important;
     }
   }
-}
-.shadowRight{
-  box-shadow: 1px 10px 1px 10px rgba(0,0,0,0.2);
-  /*z-index: 99999999;*/
-  margin-right: 1px;
 }
 </style>
 <style lang="less">
