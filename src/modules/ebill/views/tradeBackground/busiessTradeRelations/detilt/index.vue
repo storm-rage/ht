0a7512cca69936db9061d9ad2710c319e6faa57c
wangqiang <13169658096@163.com>
2022-07-15 18:37:43 +0800

    贸易-代办

---
+++ src/modules/ebill/views/tradeBackground/busiessTradeRelations/detilt/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <zj-content-container>
+    <ZjTopHeader>贸易关系详情</ZjTopHeader>
+
+    <info />
+
+    <ZjContentFooter>
+        <zj-button>返回</zj-button>
+    </ZjContentFooter>
+  </zj-content-container>
+</template>
+
+<script>
+import info from './info'
+export default {
+  components: {
+    info
+  },
+  data() {
+    return {
+      searchForm: {},
+      zjControl: {},
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/modules/ebill/views/tradeBackground/busiessTradeRelations/detilt/info.vue
@@ -0,0 +1,93 @@
+<template>
+  <zj-content-block>
+    <zj-content-block>
+      <ZjHeader>买方企业信息</ZjHeader>
+      <el-form ref="form" label-width="160px">
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="买方企业名称："> 546565465 </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="是否海天集团："> 546565465 </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="客户业务系统编码："> 546565465 </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </zj-content-block>
+
+    <zj-content-block>
+      <ZjHeader>卖方企业信息</ZjHeader>
+      <el-form ref="form" label-width="160px">
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="卖方企业名称："> 546565465 </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="客户业务系统编码："> 546565465 </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="贸易关系状态："> 546565465 </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="冻结原因："> 546565465 </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </zj-content-block>
+
+    <zj-content-block>
+      <ZjHeader>卖方银行账户信息</ZjHeader>
+      <el-form ref="form" label-width="160px">
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="银行账号："> 546565465 </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="银行账户户名："> 546565465 </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="银行账号开户行："> 546565465 </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="开户行联行号："> 546565465 </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="银行类型："> 546565465 </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+    </zj-content-block>
+
+    <zj-content-block>
+      <ZjHeader>附件信息</ZjHeader>
+      <zj-content>
+        <zj-table
+         :pager="false"
+          ref="searchTable"
+          :params="searchForm"
+          :api="zjControl.tableApi"
+        >
+          <zj-table-column prop="id" title="序号" />
+          <zj-table-column field="" title="附件名称" />
+          <zj-table-column field="" title="操作" />
+        </zj-table>
+      </zj-content>
+    </zj-content-block>
+  </zj-content-block>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchForm: {},
+      zjControl: {},
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
