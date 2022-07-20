e311938cc45c3eb420ac7c9990821247e836f22c
wangqiang <13169658096@163.com>
2022-07-20 21:04:30 +0800

    个人中心

---
+++ src/modules/base/views/sysModules/personalCenter/entInfoMaintain/index.vue
@@ -0,0 +1,269 @@
+<template>
+  <zj-content-container>
+    <zj-top-header title="企业信息维护" />
+    <!-- 企业基础信息 -->
+    <el-form ref="form" :model="form" :rules="rules" label-width="200px">
+      <zj-content-block>
+        <zj-header title="企业基础信息" />
+        <h4 class="bl zj-m-l-20 mb-10">企业信息</h4>
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="企业名称：" prop="name">
+              <el-input v-model="form.name" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="16">
+            <el-form-item label="平台客户类型：" prop="entType">
+              <span>供应商</span>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="企业简称：" prop="shortName">
+              <el-input v-model="form.shortName" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="曾用名：" prop="beforeName">
+              <el-input v-model="form.beforeName" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="客户业务系统编码：" prop="customCode">
+              <el-input v-model="form.customCode" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item
+              label="统一社会信用代码："
+              prop="bizLicence"
+              class="bizLicence"
+            >
+              <el-input v-model="form.bizLicence" :maxLength="18" />
+              {{ form.bizLicence }}
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="纳税人识别号：" prop="invoiceTaxpayerId">
+              <el-input v-model="form.invoiceTaxpayerId" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="成立日期：" prop="invoiceTaxpayerId">
+              <zj-date-picker v-model="form.expireDateStart" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="注册资本：" prop="invoiceTaxpayerId">
+              <el-input v-model="form.invoiceTaxpayerId" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="企业工商有效期：" prop="invoiceTaxpayerId">
+              <el-input v-model="form.invoiceTaxpayerId" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="企业规模：" prop="invoiceTaxpayerId">
+              <el-input v-model="form.invoiceTaxpayerId" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="企业经营类型：" prop="invoiceTaxpayerId">
+              <el-input v-model="form.invoiceTaxpayerId" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="营业执照注册地址：" prop="invoiceTaxpayerId">
+              <el-input v-model="form.invoiceTaxpayerId" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <h4 class="bl zj-m-l-20 mb-10">企业联系人</h4>
+          <el-col :span="8">
+            <el-form-item label="企业联系人姓名：" prop="fastMailName">
+              <el-input v-model="form.fastMailName" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="企业联系人手机号：" prop="fastMailPhone">
+              <el-input v-model="form.fastMailPhone" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-form-item label="企业联系地址：" prop="fastMailAddress">
+            <el-input v-model="form.fastMailAddress" :disabled="isDetail" />
+          </el-form-item>
+        </el-row>
+        <h4 class="bl zj-m-l-20 mb-10">法人信息</h4>
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="法定代表人姓名：" prop="legalPersonName">
+              <el-input v-model="form.legalPersonName" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="法定代表人手机号码：" prop="legalPersonName">
+              <el-input v-model="form.legalPersonName" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="法定代表人手机号码：" prop="legalPersonName">
+              <span>身份证</span>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="8">
+            <el-form-item label="法定代表人身份证号：" prop="legalCertNo">
+              <el-input v-model="form.legalCertNo" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item
+              label="法人身份证有效期："
+              prop="legalCertRegDate"
+              class="col-right"
+            >
+              <zj-date-range-picker
+                :clearable="false"
+                :startDate.sync="form.legalCertRegDate"
+                @startChange="valueChange('legalCertRegDate')"
+                :endDate.sync="form.legalCertExpireDate"
+                @endChange="valueChange('legalCertExpireDate')"
+                :disabled="isDetail"
+              />
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <h4 class="bl zj-m-l-20 mb-10">银行账户信息</h4>
+          <el-col :span="8">
+            <el-form-item label="银行账户名称：" prop="legalPersonName">
+              <el-input v-model="form.legalPersonName" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="银行账号：" prop="legalCertNo">
+              <el-input v-model="form.legalCertNo" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="银行账户开户行：" prop="legalCertNo">
+              <el-input v-model="form.legalCertNo" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="银行联行号：" prop="legalCertNo">
+              <el-input v-model="form.legalCertNo" :disabled="isDetail" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="8">
+            <el-form-item label="银行类型：" prop="companyType">
+              <el-select
+                v-model="form.companyType"
+                :popper-append-to-body="false"
+                :disabled="isDetail"
+              >
+                <el-option
+                  v-for="(item, index) in dictionary.companyTypeList"
+                  :key="index"
+                  :value="item.code"
+                  :label="item.desc"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </zj-content-block>
+      <!-- 企业附件 -->
+      <zj-content-block>
+        <zj-header title="企业附件" />
+        <zj-content>
+          <zj-table
+            ref="pubAttach"
+            :dataList="form.pubAttachList"
+            :pager="false"
+            keep-source
+            auto-resize
+          >
+            <zj-table-column title="附件类型">
+              <template v-slot="{ row }">
+                <span
+                  v-if="
+                    (form.entType === 'B' || form.entType === 'S') &&
+                    row.busType !== 'QYZZ' &&
+                    row.busType !== 'QYKZRZ'
+                  "
+                  class="error"
+                  >*</span
+                >
+                {{ typeMap(dictionary.busTypeList, row.busType) }}
+              </template>
+            </zj-table-column>
+            <zj-table-column field="fileName" title="附件名称" />
+            <zj-table-column title="操作">
+              <template v-slot="{ row }">
+                <zj-button
+                  type="text"
+                  class="zj-m-r-10"
+                  @click="pubAttachDownload(row)"
+                  v-if="row.fileId"
+                  >下载</zj-button
+                >
+                <zj-upload
+                  :httpRequest="pubAttachUpload"
+                  :data="{ row }"
+                  class="zj-inline"
+                  v-if="!isDetail"
+                >
+                  <zj-button type="text">上传</zj-button>
+                </zj-upload>
+              </template>
+            </zj-table-column>
+          </zj-table>
+          <zj-content>
+            <zj-content-tip
+              text="注：1.以上上传的影像件请全部加盖公司公章。"
+            ></zj-content-tip>
+            <br />
+            <zj-content-tip text="2.支持上传的文档格式：PDF。"></zj-content-tip>
+            <br />
+            <zj-content-tip
+              text="3.若上传身份证，请将身份证正、反面完整放在同一页上。请确保身份证在有效期内。 "
+            ></zj-content-tip>
+            <br />
+            <zj-content-tip
+              text=" 4.若上传营业执照，请确保营业执照为最新版本。 "
+            ></zj-content-tip>
+          </zj-content>
+        </zj-content>
+      </zj-content-block>
+    </el-form>
+
+    <zj-content-footer>
+      <zj-button type="primary" @click="submit">确认提交</zj-button>
+      <zj-button class="back" @click="back">返回</zj-button>
+    </zj-content-footer>
+  </zj-content-container>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      isDetail: this.$route.meta.pageType,
+      form: {},
+      rules: {},
+      dictionary: {},
+    };
+  },
+};
+</script>
+
+<style scoped lang="less">
+</style>
---
+++ src/modules/base/views/sysModules/personalCenter/userInfoMaintain/index.vue
@@ -0,0 +1,201 @@
+<template>
+  <zj-content-container>
+    <zj-top-header>个人信息维护</zj-top-header>
+    <zj-content-block>
+      <zj-header title="用户信息"></zj-header>
+      <zj-content>
+        <el-form ref="form" label-width="160px">
+          <el-row>
+            <el-col :span="8">
+              <el-form-item label="用户姓名：">
+                <span>xxxxxxx</span>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="角色：">
+                <el-select
+                  v-model="form.a"
+                  filterable
+                  placeholder="请选择"
+                  :popper-append-to-body="false"
+                >
+                  <el-option :label="经办员" :value="经办员" />
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="海天业务系统账号：">
+                <el-input v-model="form.a" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="证件类型：">
+                <el-select
+                  v-model="form.a"
+                  filterable
+                  placeholder="请选择"
+                  :popper-append-to-body="false"
+                  v-if="isAdd"
+                >
+                  <el-option :label="身份证号码" :value="身份证号码" />
+                </el-select>
+                <span v-else>62434343</span>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="证件号码：">
+                <el-input v-model="form.a" v-if="isAdd" />
+                <span v-else>xxxxxxx</span>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="证件有效期：">
+                <zj-date-range-picker
+                  class="el-form-item__regExpire"
+                  :startDate.sync="form.certStartDate"
+                  :endDate.sync="form.certEndDate"
+                  @startChange="certStartChange"
+                  @endChange="certEndChange"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="手机号码/用户名：">
+                <el-input v-model="form.a" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="邮箱：">
+                <el-input v-model="form.a" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="邮箱：">
+                <el-input v-model="form.a" />
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
+      </zj-content>
+    </zj-content-block>
+    <zj-content-block>
+      <zj-header title="身份证"></zj-header>
+      <zj-content>
+        <zj-table :pager="false">
+          <zj-table-column type="seq" width="60" title="序号" />
+          <zj-table-column field="field2" title="附件类型" />
+          <zj-table-column field="field2" title="附件" />
+          <zj-table-column title="操作" fixed="right">
+            <template v-slot="{ row, rowIndex }">
+              <zj-upload
+                class="zj-inline"
+                :httpRequest="handleFileUpload"
+                :autoUpload="false"
+                :onChange="handleFileChange"
+              >
+                <zj-button slot="trigger" type="text">上传</zj-button>
+              </zj-upload>
+            </template>
+            <template>
+              <zj-button v-if="row.fileId" type="text" @click="toDownload(row)"
+                >下载</zj-button
+              >
+            </template>
+          </zj-table-column>
+        </zj-table>
+        <zj-content>
+          <zj-content-tip
+            text="注：1.请将身份证正、反面完整放在同一页上，并加盖企业公章。请确保身份证在有效期内。"
+          ></zj-content-tip>
+          <br />
+          <zj-content-tip text="2.支持上传的文档格式：PDF。"></zj-content-tip>
+          <br />
+          <zj-content-tip
+            text=" 3.委托授权书请下载，加盖公司公章后上传。"
+          ></zj-content-tip>
+        </zj-content>
+      </zj-content>
+    </zj-content-block>
+    <zj-content-block>
+      <zj-content class="zj-m-l-20">
+        <h4 class="zj-m-b-20">
+          请将以上已盖章影像资料对应的纸质版邮递至以下地址：
+        </h4>
+        <el-form ref="form" label-width="100px">
+          <el-row>
+            <el-col :span="8">
+              <el-form-item label="收件人：">
+                <span>xxxxxxx</span>
+              </el-form-item>
+            </el-col>
+            <el-col :span="16">
+              <el-form-item label="收件人电话：">
+                <span>xxxxxxx</span>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="收件地址：">
+                <span>xxxxxxx</span>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
+      </zj-content>
+    </zj-content-block>
+    <zj-content-footer>
+      <zj-button type="primary" @click="submit">维护本人信息</zj-button>
+      <zj-button type="primary" @click="submit">更换操作人员</zj-button>
+      <zj-button class="back" @click="back">返回</zj-button>
+    </zj-content-footer>
+  </zj-content-container>
+</template>
+<script>
+export default {
+  components: {},
+
+  data() {
+    return {
+      form: {},
+      fileList: [
+        {
+          field1: "海天a公司",
+          field2: "是",
+          field3: "54354343423X4645656456",
+          field4: "订单保理",
+          field5: "生效",
+          field6: "2022.09.09 ～ 2023.09.08",
+          field7: "100,000,000.00",
+          field8: "生效",
+        },
+      ],
+      pageType: "detail",
+    };
+  },
+  methods: {
+    back() {},
+  },
+};
+</script>
+
+<style lang="less" scoped>
+//证件有效期
+/deep/.el-form-item__regExpire {
+  .zj-date-range-picker {
+    width: 100px;
+    .el-input__inner {
+      padding-left: 3px;
+    }
+    & ~ span {
+      display: inline-block;
+      width: 20px;
+      text-align: center;
+    }
+  }
+  .el-form-item {
+    margin-bottom: 0;
+    .el-form-item__error {
+      top: 14px;
+    }
+  }
+}
+</style>
