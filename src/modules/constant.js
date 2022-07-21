1e24b205f5a3aac13e066ce05491a8a117f32b4a
suyc <1232dev@ht.com>
2022-07-21 11:37:43 +0800

    feat:提交常量

--- src/modules/constant.js
+++ src/modules/constant.js
@@ -10,3 +10,27 @@ export const ProductType = {
   // 放贷
   JXD: 'JXD'
 }
+/***
+ * 操作标识
+ * @type {{ADD: string, UPDATE: string, DEL: string}}
+ */
+export const OperateFlag = {
+  // 新增
+  ADD: 'A',
+  //修改
+  UPDATE: 'U',
+  // 删除
+  DEL: 'D'
+}
+/**
+ * 工作流的操作标识
+ * @type {{REJECT: string, PASS: string, BACK: string}}
+ */
+export const OperResult = {
+  // 通过
+  PASS: '0',
+  // 拒绝
+  REJECT: '1',
+  // 驳回
+  BACK: '2'
+};
