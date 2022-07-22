ded4efcd51be558fa1b9e6799e8ae1c885e99731
wangxu <wangxu@izejin.com>
2022-07-22 11:11:32 +0800

    优化

--- src/modules/ebill/router/agreementManageRouter.js
+++ src/modules/ebill/router/agreementManageRouter.js
@@ -12,18 +12,19 @@ const agreementManageRouter = [
       refreshIndex: 0,
       notRequireAuth: true
     }
-  }
-  // {
-  //   path: '/billDetail',
-  //   name: 'billDetail',
-  //   component: () => import('../views/myBill/billDetail/index.vue'),
-  //   meta:{
-  //     isMock:false,
-  //     title:`凭证详情`,
-  //     keepAlive:true,
-  //     refreshIndex:0,
-  //     notRequireAuth: true
-  //   }
-  // },
+  },
+  {
+    path: '/financingAgreementManage',
+    name: 'financingAgreementManage',
+    component: () => import('../views/financingManage/agreementManage'),
+    meta:{
+      isMock: true,
+      title:`阶段性协议管理`,
+      root: '融资管理',
+      keepAlive:true,
+      refreshIndex:0,
+      notRequireAuth: true
+    }
+  },
 ]
 export default agreementManageRouter
