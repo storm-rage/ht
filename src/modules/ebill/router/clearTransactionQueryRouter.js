8da5fbd9d32ffdee58f1d94d180ba4f23279c184
wangxu <wangxu@izejin.com>
2022-07-27 16:29:26 +0800

    供应商额度，清算联调

--- src/modules/ebill/router/clearTransactionQueryRouter.js
+++ src/modules/ebill/router/clearTransactionQueryRouter.js
@@ -5,12 +5,10 @@ const clearTransactionQueryRouter = [
     name: 'queryExpireClear',
     component: () => import('../views/queryStatistics/clearTransactionQuery'),
     meta: {
-      isMock: true,
       title: `清算交易查询`,
       root: '查询统计',
       keepAlive: true,
       refreshIndex: 0,
-      notRequireAuth: true
     }
   },
   {
@@ -18,13 +16,11 @@ const clearTransactionQueryRouter = [
     name: 'queryExpireClearDetail',
     component: () => import('../views/queryStatistics/clearTransactionQuery/detail'),
     meta: {
-      isMock: true,
       title: `凭证尾款清算交易详情`,
       root: '查询统计',
       keepAlive: true,
       parent:'queryExpireClear',
       refreshIndex: 0,
-      notRequireAuth: true
     }
   },
 ]
