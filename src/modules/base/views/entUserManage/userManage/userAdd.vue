f0fe5320f4ed52ed42bd375ec8cba86f99ef5331
wangqiang <13169658096@123.com>
2022-08-05 18:24:47 +0800

    用户信息管理联调

--- src/modules/base/views/entUserManage/userManage/userAdd.vue
+++ src/modules/base/views/entUserManage/userManage/userAdd.vue
@@ -1,71 +1,70 @@
 <template>
   <zj-content-container>
-    <zj-top-header>{{
-      isAdd ? "新增用户申请" : "修改用户信息申请"
-    }}</zj-top-header>
+    <zj-top-header>新增用户申请</zj-top-header>
     <el-form ref="form" label-width="160px">
-      <zj-content-block v-if="isAdd">
+      <zj-content-block>
         <zj-header title="企业信息"></zj-header>
         <zj-content>
-          <div>
-            <el-row>
-              <el-col :span="8">
-                <el-form-item label="企业名称：">
-                  <span>xxx</span>
-                </el-form-item>
-              </el-col>
-              <el-col :span="8">
-                <el-form-item label="平台客户类型：">
-                  <span>xxxxxxx</span>
-                </el-form-item>
-              </el-col>
-              <el-col :span="8">
-                <el-form-item label="客户业务系统编码：">
-                  <span>xx</span>
-                </el-form-item>
-              </el-col>
-              <el-col :span="8">
-                <el-form-item label="是否海天集团：">
-                  <span>xx</span>
-                </el-form-item>
-              </el-col>
-            </el-row>
-          </div>
+          <el-row>
+            <el-col :span="24">
+              <el-form-item label="企业名称：">
+                <el-input v-model="detailData.name" style="width: 200px" />
+                <zj-button
+                  class="zj-m-l-20"
+                  type="primary"
+                  @click="getEnterpriseConfirm"
+                  >确认</zj-button
+                >
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="平台客户类型：">
+                <span>{{ detailData.entType | value }}</span>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="客户业务系统编码：">
+                <span>{{ detailData.customCode | value }}</span>
+              </el-form-item>
+            </el-col>
+            <el-col :span="8">
+              <el-form-item label="是否海天集团：">
+                <span>{{ detailData.isHtEnterprise | value }}</span>
+              </el-form-item>
+            </el-col>
+          </el-row>
         </zj-content>
       </zj-content-block>
       <zj-content-block>
         <zj-header title="用户信息"></zj-header>
         <zj-content>
           <el-row>
-            <el-col :span="8" v-if="isAdd">
-              <el-form-item label="用户编码：">
-                <span>xxx</span>
-              </el-form-item>
-            </el-col>
             <el-col :span="8">
               <el-form-item label="用户姓名：">
-                <el-input v-model="form.a" v-if="isAdd" />
-                <span v-else>xxxxxxx</span>
+                <el-input v-model="form.userName" />
               </el-form-item>
             </el-col>
             <el-col :span="8">
               <el-form-item label="证件类型：">
                 <el-select
-                  v-model="form.a"
+                  v-model="form.certType"
                   filterable
                   placeholder="请选择"
                   :popper-append-to-body="false"
-                  v-if="isAdd"
                 >
-                  <el-option :label="身份证号码" :value="身份证号码" />
+                  <!-- <el-option
+              v-for="item in dictionary.isGenerateVouchers"
+              :key="item.code"
+              :label="item.desc"
+              :value="item.code"
+            >
+            </el-option> -->
                 </el-select>
-                <span v-else>62434343</span>
               </el-form-item>
             </el-col>
             <el-col :span="8">
               <el-form-item label="证件号码：">
-                <el-input v-model="form.a" v-if="isAdd" />
-                <span v-else>xxxxxxx</span>
+                <el-input v-model="form.certNo" />
               </el-form-item>
             </el-col>
             <el-col :span="8">
@@ -74,22 +73,20 @@
                   class="el-form-item__regExpire"
                   :startDate.sync="form.certStartDate"
                   :endDate.sync="form.certEndDate"
-                  @startChange="certStartChange"
-                  @endChange="certEndChange"
                 />
               </el-form-item>
             </el-col>
             <el-col :span="8">
               <el-form-item label="手机号码/用户名：">
-                <el-input v-model="form.a" />
+                <el-input v-model="form.mobileNo" />
               </el-form-item>
             </el-col>
             <el-col :span="8">
               <el-form-item label="邮箱：">
-                <el-input v-model="form.a" />
+                <el-input v-model="form.email" />
               </el-form-item>
             </el-col>
-            <el-col :span="8" v-if="isAdd">
+            <el-col :span="8">
               <el-form-item label="海天业务系统账号：">
                 <el-input v-model="form.a" />
               </el-form-item>
@@ -97,12 +94,18 @@
             <el-col :span="24">
               <el-form-item label="用户角色：">
                 <el-select
-                  v-model="form.a"
+                  v-model="form.roleId"
                   filterable
                   placeholder="请选择"
                   :popper-append-to-body="false"
                 >
-                  <el-option :label="身份证号码" :value="身份证号码" />
+                  <!-- <el-option
+              v-for="item in dictionary.isGenerateVouchers"
+              :key="item.code"
+              :label="item.desc"
+              :value="item.code"
+            >
+            </el-option> -->
                 </el-select>
               </el-form-item>
             </el-col>
@@ -117,21 +120,31 @@
                   placeholder="请选择"
                   :popper-append-to-body="false"
                 >
-                  <el-option :label="身份证号码" :value="身份证号码" />
+                  <!-- <el-option
+              v-for="item in dictionary.isGenerateVouchers"
+              :key="item.code"
+              :label="item.desc"
+              :value="item.code"
+            >
+            </el-option> -->
                 </el-select>
               </el-form-item>
             </el-col>
           </el-row>
           <zj-collapse title="身份证">
-            <zj-table :pager="false">
+            <zj-table :pager="false" ref="attach" :dataList="attachInfo">
               <zj-table-column type="seq" width="60" title="序号" />
-              <zj-table-column field="field2" title="附件类型" />
-              <zj-table-column field="field2" title="附件" />
+              <zj-table-column field="type" title="附件类型" />
+              <zj-table-column field="fileName" title="附件" />
               <zj-table-column title="操作" fixed="right">
                 <template v-slot="{ row }">
-                  <zj-button type="text" @click="toDownload(row)"
-                    >下载</zj-button
+                  <zj-upload
+                    :httpRequest="handleFileUpload"
+                    :data="{ row }"
+                    class="zj-inline"
                   >
+                    <zj-button type="text">上传</zj-button>
+                  </zj-upload>
                 </template>
               </zj-table-column>
             </zj-table>
@@ -144,27 +157,6 @@
         </zj-content>
       </zj-content-block>
     </el-form>
-    <zj-content-block v-if="!isAdd">
-      <zj-header title="用户归属企业信息"></zj-header>
-      <zj-content>
-        <zj-table :dataList="fileList">
-          <zj-table-column field="field1" title="企业代码" />
-          <zj-table-column field="field2" title="客户业务系统编码" />
-          <zj-table-column field="field3" title="企业名称" />
-          <zj-table-column field="field3" title="是否海天集团" />
-          <zj-table-column field="field3" title="平台客户类型" />
-          <zj-table-column field="field3" title="创建日期" />
-          <zj-table-column field="field3" title="企业状态" />
-          <zj-table-column field="field3" title="用户状态" />
-          <zj-table-column field="field4" title="角色" />
-          <zj-table-column title="操作" fixed="right" width="200px">
-            <template>
-              <zj-button type="text" @click="&quot;&quot;;">维护角色</zj-button>
-            </template>
-          </zj-table-column>
-        </zj-table>
-      </zj-content>
-    </zj-content-block>
     <zj-content-footer>
       <zj-button type="primary" @click="submit">提交申请</zj-button>
       <zj-button class="back" @click="goParent">返回</zj-button>
@@ -177,23 +169,49 @@ export default {
 
   data() {
     return {
-      isAdd: this.$route.name === "userAdd" ? true : false,
+      zjControl: this.$api.userInfoManage,
       form: {},
-      fileList: [
-        {
-          field1: "海天a公司",
-          field2: "是",
-          field3: "54354343423X4645656456",
-          field4: "订单保理",
-          field5: "生效",
-          field6: "2022.09.09 ～ 2023.09.08",
-          field7: "100,000,000.00",
-          field8: "生效",
-        },
-      ],
+      detailData: {},
+      attachInfo: [{ fileId: "", type: "身份证影印件", fileName: "" }],
     };
   },
+  created() {
+    this.getRow();
+    this.getUserInformation();
+  },
   methods: {
+    //获取企业信息-确认
+    getEnterpriseConfirm() {
+      this.zjControl
+        .getEnterpriseConfirm({ name: this.detailData.name })
+        .then((res) => {
+          this.detailData = res.data;
+        });
+    },
+    //上传附件
+    handleFileUpload({ file, data }) {
+      console.log(file, data);
+      let formData = new FormData();
+      formData.append("fileId", file.uid);
+      formData.append("fileName", file.name);
+      formData.append("fileSize", file.size);
+      formData.append("fileType", file.type);
+      this.zjControl.uploadAttach(formData).then((res) => {
+        data.row.fileId = res.data.fileId;
+        data.row.fileName = res.data.fileName;
+        this.$message.success("附件上传成功!");
+      });
+    },
+    // 新增用户
+    submit() {
+      this.form.attachName = this.attachInfo[0].fileName
+      this.form.fileId = this.attachInfo[0].fileName
+      this.form.id = this.attachInfo[0].id
+      this.zjControl.addUser(this.form).then((res) => {
+        this.$message.success("新增用户成功！");
+        this.goParent();
+      });
+    },
   },
 };
 </script>
--- src/modules/base/views/entUserManage/userManage/vindicate/index.vue
+++ src/modules/base/views/entUserManage/userManage/vindicate/index.vue
@@ -1,6 +1,8 @@
 <template>
   <zj-content-container>
-    <zj-top-header title="维护用户信息"></zj-top-header>
+    <zj-top-header>
+      {{ pageType === "detail" ? "用户详情" : "维护用户信息" }}
+    </zj-top-header>
     <el-form ref="form" label-width="180px">
       <zj-content-block>
         <zj-header title="用户信息"></zj-header>
@@ -19,32 +21,35 @@
               </el-col>
               <el-col :span="8">
                 <el-form-item label="证件类型：">
-                  <span>{{ detailData.aaa | value }}</span>
+                  <span>{{ detailData.certType | value }}</span>
                 </el-form-item>
               </el-col>
               <el-col :span="8">
                 <el-form-item label="证件号码：">
-                  <span>{{ detailData.aaa | value }}</span>
+                  <span>{{ detailData.certNo | value }}</span>
                 </el-form-item>
               </el-col>
               <el-col :span="8">
                 <el-form-item label="证件有效期：">
-                  <span>{{ detailData.aaa | value }}</span>
+                  <span
+                    >{{ detailData.certStartDate | value }} 至
+                    {{ detailData.certEndDate | value }}</span
+                  >
                 </el-form-item>
               </el-col>
               <el-col :span="8">
                 <el-form-item label="手机号码/用户名：">
-                  <span>{{ detailData.aaa | value }}</span>
+                  <span>{{ detailData.mobileNo | value }}</span>
                 </el-form-item>
               </el-col>
               <el-col :span="8">
                 <el-form-item label="邮箱：">
-                  <span>{{ detailData.aaa | value }}</span>
+                  <span>{{ detailData.email | value }}</span>
                 </el-form-item>
               </el-col>
               <el-col :span="8">
                 <el-form-item label="海天业务系统账号：">
-                 <span>{{ detailData.aaa | value }}</span>
+                  <span>{{ detailData.htSysCode | value }}</span>
                 </el-form-item>
               </el-col>
             </el-row>
@@ -54,20 +59,21 @@
       <zj-content-block>
         <zj-header title="用户归属企业信息"></zj-header>
         <zj-content>
-          <zj-table
-            ref="searchTable"
-            :params="searchForm"
-            :api="zjControl.queryUserPage"
-          >
-            <zj-table-column field="field1" title="企业代码" />
-            <zj-table-column field="field2" title="客户业务系统编码" />
-            <zj-table-column field="field3" title="企业名称" />
-            <zj-table-column field="field3" title="是否海天集团" />
-            <zj-table-column field="field3" title="平台客户类型" />
-            <zj-table-column field="field3" title="新增日期" />
-            <zj-table-column field="field3" title="企业状态" />
-            <zj-table-column field="field3" title="用户状态" />
-            <zj-table-column field="field4" title="角色" />
+          <zj-table ref="searchTable" :dataList="detailData.userAndEntInfoList">
+            <zj-table-column field="code" title="企业代码" />
+            <zj-table-column field="customCode" title="客户业务系统编码" />
+            <zj-table-column field="entName" title="企业名称" />
+            <zj-table-column field="isHtEnterprise" title="是否海天集团" />
+            <zj-table-column field="entType" title="平台客户类型" />
+            <zj-table-column field="createDatetime" title="新增日期" />
+            <zj-table-column field="entState" title="企业状态" />
+            <zj-table-column field="roleId" title="角色" />
+            <zj-table-column field="userState" title="状态" />
+            <zj-table-column
+              field="field3"
+              title="支持开立债权凭证的对账单类型"
+              v-if="pageType !== 'detail'"
+            />
             <zj-table-column
               title="操作"
               fixed="right"
@@ -75,9 +81,14 @@
               v-if="pageType !== 'detail'"
             >
               <template>
-                <zj-button type="text" @click="&quot;&quot;;">制key</zj-button>
-                <zj-button type="text" @click="&quot;&quot;;">冻结</zj-button>
-                <zj-button type="text" @click="&quot;&quot;;">注销</zj-button>
+                <zj-button type="text" @click="makeCertKey(row)"
+                  >制key</zj-button
+                >
+                <zj-button type="text" @click="freezeUser(row)">冻结</zj-button>
+                <zj-button type="text" @click="unfreezeUser(row)"
+                  >解冻</zj-button
+                >
+                <zj-button type="text" @click="cancelUser(row)">注销</zj-button>
               </template>
             </zj-table-column>
           </zj-table>
@@ -85,10 +96,13 @@
       </zj-content-block>
     </el-form>
     <zj-content-footer>
-      <zj-button type="primary" @click="back" v-if="pageType !== 'detail'"
+      <zj-button
+        type="primary"
+        @click="goChild('userUpdate', row)"
+        v-if="pageType !== 'detail'"
         >修改</zj-button
       >
-      <zj-button class="back" @click="goParent">返回</zj-button>
+      <zj-button @click="goParent">返回</zj-button>
     </zj-content-footer>
   </zj-content-container>
 </template>
@@ -97,9 +111,7 @@ export default {
   components: {},
   data() {
     return {
-      zjControl: {
-        getUserInformation: this.$api.userInfoManage.getUserInformation,
-      },
+      zjControl: this.$api.userInfoManage,
       pageType: this.$route.meta.pageType,
       detailData: {},
     };
@@ -113,6 +125,82 @@ export default {
     getUserInformation() {
       this.zjControl.getUserInformation({ id: this.row.id }).then((res) => {
         this.detailData = res.data;
+      });
+    },
+    //制key
+    makeCertKey(row) {
+      this.$messageBox({
+        type: "confirm",
+        title: `制key确认`,
+        content: `${"是否确认为用户" + row.userName + "  -  制作key"}`,
+        messageResolve: () => {
+          if (row.certType === "1") {
+            let paramsKey = {
+              userId: row.id,
+              p10: "",
+            };
+            this.zjControl.makeCertKey(paramsKey).then(() => {
+              this.$Message.success(`已为用户：${row.userName}  -  制Key成功`);
+              this.search(false);
+            });
+          } else if (row.certType === "2") {
+            let paramsClound = {
+              entId: row.entId,
+              userId: row.id,
+            };
+            this.zjControl.bindCloudCerUser(paramsClound).then(() => {
+              this.$Message.success(
+                `已为用户：${row.userName}  -  绑定云证书成功！`
+              );
+              this.search(false);
+            });
+          }
+        },
+      });
+    },
+    //账户冻结
+    freezeUser(row) {
+      this.$messageBox({
+        type: "confirm",
+        title: `温馨提示`,
+        content: `是否确认冻结该用户？`,
+        showCancelButton: true,
+        messageResolve: () => {
+          this.zjControl.freezeUser({ id: row.id }).then(() => {
+            this.getNewUserInfo(true);
+            this.$message.success("冻结账户成功！");
+          });
+        },
+      });
+    },
+    //账户解冻
+    unfreezeUser(row) {
+      this.$messageBox({
+        type: "confirm",
+        title: `温馨提示`,
+        content: `是否确认解冻该用户？`,
+        showCancelButton: true,
+        messageResolve: () => {
+          this.zjControl.unfreezeUser({ id: row.id }).then(() => {
+            this.getNewUserInfo(true);
+            this.$message.success("解冻账户成功！");
+          });
+        },
+      });
+    },
+    //账户注销
+    cancelUser(row) {
+      this.$messageBox({
+        type: "confirm",
+        title: `温馨提示`,
+        content: `是否确认注销该用户？`,
+        showCancelButton: true,
+        messageResolve: () => {
+          this.zjControl.cancelUser({ id: row.id }).then(() => {
+            this.getNewUserInfo(true);
+            this.$message.success("注销账户成功！");
+          });
+        },
       });
     },
   },
