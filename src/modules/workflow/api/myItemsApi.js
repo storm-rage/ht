892a8f53713d31856784e6f6728f179053fc4eab
wangqiang <13169658096@123.com>
2022-08-18 19:48:41 +0800

    待办联调

--- src/modules/workflow/api/myItemsApi.js
+++ src/modules/workflow/api/myItemsApi.js
@@ -1,9 +1,17 @@
 //我的代办
 import request from '@common/axios/request'
 const myItems = {
-  //列表查询
-  queryReceivedBillList: (params) => {
-    return request.get('/my-bill/query-receive-bill-list',{params})
+  //待办
+  myTodoList: (params) => {
+    return request.get('/my-todo/todo-list', { params })
+  },
+  //已办
+  myTodoDoneList: (params) => {
+    return request.get('/my-todo/done-list', { params })
+  },
+  //已办结
+  myTodoDoneFinishedList: (params) => {
+    return request.get('/my-todo/done-finished-list', { params })
   }
 }
 export default myItems
