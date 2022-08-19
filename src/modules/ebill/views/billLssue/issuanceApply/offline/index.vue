031ff172952073a48e00adc5cab0ae4fb2bb285b
刘渤轩 <wliuboxuan@haday.cn>
2022-08-19 15:57:02 +0800

    feat: 凭证签发申请联调完成

--- src/modules/ebill/views/billLssue/issuanceApply/offline/index.vue
+++ src/modules/ebill/views/billLssue/issuanceApply/offline/index.vue
@@ -18,32 +18,32 @@
         <el-form ref="searchForm" :model="searchForm">
           <el-form-item label="对账单编号：">
             <el-input
-              v-model="searchForm.issueEntName"
+              v-model="searchForm.acctBillCode"
               @keyup.enter.native="enterSearch"
             />
           </el-form-item>
           <el-form-item label="供应商业务系统编码：">
             <el-input
-              v-model="searchForm.issueEntName"
+              v-model="searchForm.supplierCode"
               @keyup.enter.native="enterSearch"
             />
           </el-form-item>
           <el-form-item label="供应商名称：">
             <el-input
-              v-model="searchForm.issueEntName"
+              v-model="searchForm.supplierName"
               @keyup.enter.native="enterSearch"
             />
           </el-form-item>
           <el-form-item label="对账日期：" class="col-right">
             <zj-date-range-picker
-              :startDate.sync="searchForm.expireDateStart"
-              :endDate.sync="searchForm.expireDateEnd"
+              :startDate.sync="searchForm.checkBillDateStart"
+              :endDate.sync="searchForm.checkBillDateEnd"
             />
           </el-form-item>
           <el-form-item label="预计付款日期：" class="col-right">
             <zj-date-range-picker
-              :startDate.sync="searchForm.expireDateStart"
-              :endDate.sync="searchForm.expireDateEnd"
+              :startDate.sync="searchForm.estimatedPaymentDateStart"
+              :endDate.sync="searchForm.estimatedPaymentDateEnd"
             />
           </el-form-item>
         </el-form>
@@ -51,26 +51,31 @@
       <div class="zj-search-response">
         <zj-table
           :api="zjControl.offlineList"
-          ref="selectionCheckbox"
+          :params="searchForm"
+          ref="searchTable"
           @checkbox-change="checkChange"
           @checkbox-all="checkChange"
         >
-          <zj-table-column type="checkbox" />
+          <zj-table-column type="checkbox" width="60" />
           <zj-table-column field="acctBillCode" title="对账单编号" />
-          <zj-table-column field="supplierCode" title="供应商编码" />
+          <zj-table-column field="supplierCode" title="供应商业务系统编码" />
           <zj-table-column field="supplierName" title="供应商名称" />
           <zj-table-column
             field="checkBillDate"
             title="对账日期"
             :formatter="date"
           />
-          <zj-table-column field="checkBillAmt" title="对账单金额" :formatter="money"/>
+          <zj-table-column
+            field="checkBillAmt"
+            title="对账单金额"
+            :formatter="money"
+          />
           <zj-table-column
             field="estimatedPaymentDate"
             title="预计付款日期"
             :formatter="date"
           />
-          <zj-table-column field="voucherRemark" title="开立凭证说明" />
+          <zj-table-column field="voucherRemark" title="备注" />
           <zj-table-column title="操作" fixed="right">
             <template v-slot="{ row }">
               <zj-button type="text" @click="toDatails(row)"
@@ -88,7 +93,7 @@
       <!-- 工作流 -->
       <zj-workflow>
         <el-row slot="right">
-          <zj-button @click="goChild" :api="zjBtn.passBillSignBatch"
+          <zj-button @click="toIssuance" :api="zjBtn.passBillSignBatch"
             >签发凭证</zj-button
           >
           <zj-button @click="remove" :api="zjBtn.passBillSignBatch"
@@ -101,7 +106,7 @@
 </template>
 <script>
 export default {
-  data() {
+  data () {
     return {
       zjControl: {
         offlineList: this.$api.openBillApply.offlineList, //线下对账单-查询
@@ -109,77 +114,89 @@ export default {
         billExcel: this.$api.openBillApply.billExcel, //上传开立凭证文件
         billData: this.$api.openBillApply.billData, //上传开立凭证文件-保存数据
         accountOffline: this.$api.openBillApply.accountOffline, //线下对账单-删除
-        billApply: this.$api.openBillApply.billApply, //对账单-签发凭证
+        billApply: this.$api.openBillApply.billApply //对账单-签发凭证
       },
       searchForm: {},
-      selection: [], //储存删除的值
-      excelList: {},
-    };
+      selection: [], // 勾选的数据
+      excelList: {}
+    }
   },
-  created() {
-    this.getApi();
+  created () {
+    this.getApi()
   },
   methods: {
     //点击下载
-    downs(row) {
-      this.zjControl.openBill().then((res) => {
-        if (res.code === 200) {
-        }
-      });
-    },
-    //上传
-    upLoad({ file }) {
-      let formData = new FormData();
-      formData.append("fileOB", file);
+    downs (row) {
       this.zjControl
-        .billExcel(formData)
-        .then((ret) => {
-          if (ret.code == 200) {
-            this.excelList = ret.data;
-            this.$message.success("上传成功!");
+        .openBill()
+        .then(res => {
+          if (res.code === 200) {
           }
         })
-        .catch(() => {});
+        .catch(() => {})
+    },
+    //上传
+    upLoad ({ file }) {
+      let formData = new FormData()
+      formData.append('fileOB', file)
+      this.zjControl
+        .billExcel(formData)
+        .then(ret => {
+          if (ret.code == 200) {
+            this.excelList = ret.data
+            this.$message.success('上传成功!')
+          }
+        })
+        .catch(() => {})
     },
     //签发凭证
-    goChild(row) {
+    toIssuance () {
       let params = {
-        ApiModelProperty: '1',
-      };
-      this.zjControl.billApply(params).then((res) => {
+        applyType: '1',
+        accountBillList: this.selection
+      }
+      this.zjControl.billApply(params).then(res => {
         if (res.code === 200) {
-          this.$router.push("/openBillApplyConfirm");
+          this.goChild('openBillApplyConfirm', {
+            list: res.data.accountBillList || [],
+            applyType: '1' // 线下
+          })
         }
-      });
+      })
     },
     //维护附件
-    toDatails(row) {
-      this.$router.push("/openBillApplyConfirm");
+    toDatails (row) {
+      this.$router.push({
+        name: 'offlineDatails',
+        params: {
+          rowData: row
+        }
+      })
     },
     //勾选
-    checkChange(row) {
-      let checkArr = this.$refs.selectionCheckbox.getCheckboxRecords();
+    checkChange (row) {
+      let checkArr = this.$refs.searchTable.getCheckboxRecords()
       // console.log(checkArr, "===勾选===");
-      this.selection = checkArr;
+      this.selection = checkArr
       // console.log(this.selection, "===储存勾选===");
     },
     //勾选删除
-    remove() {
-      this.$refs.selectionCheckbox.removeCheckboxRow();
+    remove () {
+      this.$refs.searchTable.removeCheckboxRow()
       let params = {
-        req: this.selection,
-      };
+        req: this.selection
+      }
       this.zjControl
         .accountOffline(params)
-        .then((ret) => {
+        .then(ret => {
           if (ret.code == 200) {
-            this.$message.success("删除成功!");
+            this.$message.success('删除成功!')
           }
         })
-        .catch(() => {});
-    },
-  },
-};
+        .catch(() => {})
+    }
+  }
+}
 </script>
 <style lang="less" scoped>
 .zj-search-condition {
--- src/modules/ebill/views/billLssue/issuanceApply/onLine/index.vue
+++ src/modules/ebill/views/billLssue/issuanceApply/onLine/index.vue
@@ -5,49 +5,62 @@
       <template slot="searchForm">
         <el-form ref="searchForm" :model="searchForm">
           <el-form-item label="对账单编号：" class="col-center">
-            <el-input v-model="searchForm.ebillCode" />
+            <el-input
+              v-model.trim="searchForm.acctBillCode"
+              clearable
+              @keyup.enter.native="search"
+            />
           </el-form-item>
-          <el-form-item label="供应商编码：" class="col-center">
-            <el-input v-model="searchForm.ebillCode" />
+          <el-form-item label="供应商业务系统编码" class="col-center">
+            <el-input
+              v-model.trim="searchForm.supplierCode"
+              clearable
+              @keyup.enter.native="search"
+            />
           </el-form-item>
           <el-form-item label="供应商名称：" class="col-center">
-            <el-input v-model="searchForm.ebillCode" />
+            <el-input
+              v-model.trim="searchForm.supplierName"
+              clearable
+              @keyup.enter.native="search"
+            />
           </el-form-item>
           <el-form-item label="对账日期：" class="col-right">
             <zj-date-range-picker
-              :startDate.sync="searchForm.expireDateStart"
-              :endDate.sync="searchForm.expireDateEnd"
+              :startDate.sync="searchForm.checkBillDateStart"
+              :endDate.sync="searchForm.checkBillDateEnd"
             />
           </el-form-item>
-          <el-form-item label="付款日期：" class="col-right">
+          <el-form-item label="预计付款日期：" class="col-right">
             <zj-date-range-picker
-              :startDate.sync="searchForm.expireDateStart"
-              :endDate.sync="searchForm.expireDateEnd"
+              :startDate.sync="searchForm.estimatedPaymentDateStart"
+              :endDate.sync="searchForm.estimatedPaymentDateEnd"
             />
           </el-form-item>
           <el-form-item label="是否开立凭证：" class="col-center">
             <el-select
-              v-model="searchForm.isGenerateVoucher"
+              v-model="searchForm.isIssueVoucher"
               placeholder="请选择"
               clearable
               :popper-append-to-body="false"
             >
               <el-option value="" label="全部"></el-option>
-              <!-- <el-option
-              v-for="item in dictionary.isGenerateVouchers"
-              :key="item.code"
-              :label="item.desc"
-              :value="item.code"
-            >
-            </el-option> -->
+              <el-option
+                v-for="item in dictionary.isIssueVoucher"
+                :key="item.code"
+                :label="item.desc"
+                :value="item.code"
+              >
+              </el-option>
             </el-select>
           </el-form-item>
         </el-form>
       </template>
       <div class="zj-search-response">
         <zj-table
-          ref="openBillApply"
+          ref="searchTable"
           :api="zjControl.onlineList"
+          :params="searchForm"
           keep-source
           @checkbox-change="checkChange"
           @checkbox-all="checkChange"
@@ -56,47 +69,89 @@
             mode: 'row',
             icon: '-',
             autoClear: false,
-            showStatus: true,
+            showStatus: true
           }"
         >
-          <zj-table-column type="checkbox"/>
+          <zj-table-column type="checkbox" width="60" />
           <zj-table-column field="acctBillCode" title="对账单编号" />
           <zj-table-column field="companyName" title="买方名称" />
-          <zj-table-column field="billSource" title="供应商业务系统编码" />
-          <zj-table-column field="checkBillAmt" title="供应商名称" />
+          <zj-table-column
+            field="supplierCode"
+            title="供应商业务系统编码"
+            width="140"
+          />
+          <zj-table-column field="supplierName" title="供应商名称" />
           <zj-table-column
             field="checkBillDate"
             title="对账日期"
+            width="100"
             :formatter="date"
           />
-          <zj-table-column field="inputDate" title="入库日期/放行日期" />
+          <zj-table-column
+            field="inputDate"
+            title="入库日期/放行日期"
+            width="200"
+          >
+            <template v-slot="{ row }">
+              <div v-if="row.inputDate || row.passDate">
+                {{ dateFormat(row.inputDate) }} ~ {{ dateFormat(row.passDate) }}
+              </div>
+              <div v-else>-</div>
+            </template>
+          </zj-table-column>
           <zj-table-column
             field="estimatedPaymentDate"
             title="预计付款日期"
             :formatter="date"
           >
             <template v-slot="{ row }">
-              <div v-if="$refs.openBillApply.isActiveByRow(row)">
+              <div v-if="$refs.searchTable.isActiveByRow(row)">
                 <zj-date-picker
                   class="table-date-picker"
                   :date.sync="row.estimatedPaymentDate"
                 />
               </div>
-              <div v-else>{{date(row.estimatedPaymentDate)}}</div>
+              <div v-else>{{ date(row.estimatedPaymentDate) }}</div>
             </template>
           </zj-table-column>
-          <zj-table-column field="checkBillAmt" title="对账单金额"/>
-          <zj-table-column field="openBillAmt" title="开单金额" :edit-render="{ name: '$input' }"/>
-          <zj-table-column field="isIssueVoucher" title="是否开立凭证" />
+          <zj-table-column
+            field="checkBillAmt"
+            title="对账单金额"
+            width="120"
+          />
+          <zj-table-column field="openBillAmt" title="开单金额" width="120">
+            <template v-slot="{ row }">
+              <div v-if="$refs.searchTable.isActiveByRow(row)">
+                <vxe-input
+                  v-model="row.openBillAmt"
+                  type="float"
+                  digits="2"
+                  :controls="false"
+                  style="width: 100%;"
+                ></vxe-input>
+              </div>
+              <div v-else>{{ money(row.openBillAmt) }}</div>
+            </template>
+          </zj-table-column>
+          <zj-table-column
+            field="isIssueVoucher"
+            title="是否开立凭证"
+            width="100"
+            :formatter="obj => ifOrNot(obj.cellValue)"
+          />
           <zj-table-column field="billSource" title="对账单来源" />
-          <zj-table-column field="voucherRemark" title="开立凭证说明" :edit-render="{ name: '$input' }"/>
+          <zj-table-column
+            field="voucherRemark"
+            title="备注"
+            :edit-render="{ name: '$input' }"
+          />
           <zj-table-column title="操作" fixed="right">
             <template v-slot="{ row, rowIndex }">
-              <template v-if="!$refs.openBillApply.isActiveByRow(row)">
+              <template v-if="!$refs.searchTable.isActiveByRow(row)">
                 <zj-button type="text" @click="minute(row)">详情</zj-button>
                 <zj-button type="text" @click="edit(row)">修改</zj-button>
               </template>
-              <template v-if="$refs.openBillApply.isActiveByRow(row)">
+              <template v-if="$refs.searchTable.isActiveByRow(row)">
                 <zj-button type="text" @click="save(row, rowIndex)"
                   >保存</zj-button
                 >
@@ -111,7 +166,7 @@
         <zj-content style="padding-top: 0">
           <zj-content-tip
             text="注：1.预计还款日期为平台根据结算周期计算所得，支持修改。
-                    2.点击修改，可修改预计付款日、开单金额和开立凭证说明
+                    2.点击修改，可修改预计付款日、开单金额和备注
                     3.凭证到期日=预计付款日+开单宽限期限。"
           ></zj-content-tip>
         </zj-content>
@@ -119,7 +174,7 @@
       <!-- 工作流 -->
       <zj-workflow v-model="workflow">
         <el-row slot="right">
-          <zj-button @click="goChild" :api="zjBtn.passBillSignBatch"
+          <zj-button @click="toIssuance" :api="zjBtn.passBillSignBatch"
             >签发凭证</zj-button
           >
         </el-row>
@@ -129,105 +184,121 @@
 </template>
 <script>
 // import { OperateFlag } from "@modules/constant.js";
+import { formatDate } from '@utils/index'
 export default {
-  data() {
+  data () {
     return {
-      workflow: "",
+      workflow: '',
       zjControl: {
         onlineList: this.$api.openBillApply.onlineList, //线上对账单-查询
         onlineDetail: this.$api.openBillApply.onlineDetail, //线上对账单-详情
         billApply: this.$api.openBillApply.billApply, //对账单-签发凭证
-        updateStatementOnline: this.$api.openBillApply.updateStatementOnline, // 修改
+        updateStatementOnline: this.$api.openBillApply.updateStatementOnline // 修改
       },
-      searchForm: {},
+      searchForm: {
+        acctBillCode: '',
+        supplierCode: '',
+        supplierName: '',
+        checkBillDateStart: '',
+        checkBillDateEnd: '',
+        estimatedPaymentDateStart: '',
+        estimatedPaymentDateEnd: '',
+        isIssueVoucher: ''
+      },
+      dictionary: {
+        isIssueVoucher: [
+          { desc: '是', code: '1' },
+          { desc: '否', code: '0' }
+        ]
+      },
       onlineDetailLsit: {}, //存储详情数据
       openBillApplyRow: {}, //编辑当前行
-      list: [],
-    };
+      list: []
+    }
   },
-  created() {
-    this.getApi();
+  created () {
+    this.getApi()
   },
   methods: {
+    dateFormat (time) {
+      if (!time) return '-'
+      let dt = typeof time === 'number' ? time : Number(time)
+      return dt.length === 8 ? this.date(dt) : formatDate(dt, 'yyyy-MM-dd')
+    },
     //详情
-    minute(row) {
-      // console.log(row, "详情");
-      this.zjControl.onlineDetail({ id: row.id }).then((res) => {
-        if (res.code === 200) {
-          // console.log(res.data, "data");
-          this.$router.push({
-            name: "openBillApplyDetails",
-            params: { rowData: res.data },
-          });
-        }
-      });
-      // console.log(this.onlineDetailLsit, "传递之前");
-      // return;
+    minute (row) {
+      this.$router.push({
+        name: 'openBillApplyDetails',
+        query: { rowId: row.id }
+      })
     },
     //修改单元格
-    edit(row) {
-      console.log(row, "修改");
+    edit (row) {
+      console.log(row, '修改')
       if (!this.isTableEdit()) {
-        return;
+        return
       }
-      this.openBillApplyRow = Object.assign({}, row);
-      this.$refs.openBillApply.setActiveRow(row);
+      // this.openBillApplyRow = Object.assign({}, row)
+      this.openBillApplyRow = JSON.parse(JSON.stringify(row))
+      this.$refs.searchTable.setActiveRow(row)
     },
-    isTableEdit() {
-      let key = true;
-      if (
-        this.$refs.openBillApply &&
-        this.$refs.openBillApply.getActiveRecord()
-      ) {
-        key = false;
+    isTableEdit () {
+      let key = true
+      if (this.$refs.searchTable && this.$refs.searchTable.getActiveRecord()) {
+        key = false
       }
       if (!key) {
-        this.$log.alert("请保存当前正在编辑的数据");
+        this.$log.alert('请保存当前正在编辑的数据')
       }
-      return key;
+      return key
     },
     //保存单元格
-    save(row) {
-      this.form = { ...this.form, ...row };
-      this.zjControl.updateStatementOnline({
-        id: row.id,
-        acctBillCode: row.acctBillCode,
-        estimatedPaymentDate: row.estimatedPaymentDate,
-        voucherRemark: row.openBillAmt,
-        payableAmvoucherRemarkt: row.voucherRemark,
-      })
-        .then(res=>{
-          this.$refs.openBillApply.clearActived();
-          this.$message.success("保存成功");   
+    save (row) {
+      this.form = { ...this.form, ...row }
+      this.zjControl
+        .updateStatementOnline({
+          id: row.id,
+          acctBillCode: row.acctBillCode,
+          estimatedPaymentDate: row.estimatedPaymentDate,
+          // payableAmt: row.openBillAmt,
+          openBillAmt: row.openBillAmt,
+          voucherRemark: row.voucherRemark
         })
-        .catch(()=>{})
+        .then(res => {
+          this.$refs.searchTable.clearActived()
+          this.$message.success('保存成功')
+        })
+        .catch(() => {})
     },
     //取消单元格
-    cancel() {
-      this.$refs.openBillApply.clearActived();
+    cancel (row) {
+      row = Object.assign({}, this.openBillApplyRow)
+      this.$refs.searchTable.clearActived()
     },
     //勾选
-    checkChange() {
-      let checkArr = this.$refs.openBillApply.getCheckboxRecords();
-      console.log(checkArr, "勾选的值11111");
-      this.list = checkArr;
-      console.log(this.list, "勾选的值22222");
+    checkChange () {
+      let checkArr = this.$refs.searchTable.getCheckboxRecords()
+      // console.log(checkArr, '勾选的值11111')
+      this.list = checkArr
+      // console.log(this.list, '勾选的值22222')
     },
     //签发凭证
-    goChild(row) {
-      // console.log(row, "=======");
+    toIssuance (row) {
       let params = {
-        applyType: "0",
-        accountBillList: this.list,
-      };
-      this.zjControl.billApply(params).then((res) => {
+        applyType: '0',
+        accountBillList: this.list
+      }
+      this.zjControl.billApply(params).then(res => {
         if (res.code === 200) {
-          this.$router.push("/openBillApplyConfirm");
+          this.goChild('openBillApplyConfirm', {
+            list: res.data.accountBillList || [],
+            applyType: '1' // 线下
+          })
         }
-      });
-    },
-  },
-};
+      })
+    }
+  }
+}
 </script>
 <style lang="less" scoped>
 .zj-search-condition {
@@ -235,13 +306,14 @@ export default {
   margin-left: 20px;
 }
 .zj-search-response {
-  /deep/ .is--controls {
+  /deep/ .is--controls,
+  .vxe-input {
     line-height: 2.5;
   }
 }
 .table-date-picker {
   width: 100%;
-  /deep/ .el-input__inner{
+  /deep/ .el-input__inner {
     width: 100%;
     min-width: auto;
   }
