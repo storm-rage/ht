e1659f7932bf1c327c7c64d7958a8ab80bb1fca8
suyc <1232dev@ht.com>
2022-08-23 11:17:23 +0800

    feat:修改代办

--- src/modules/workflow/constant.js
+++ src/modules/workflow/constant.js
@@ -28,35 +28,104 @@ export const myItemsPath = {
 
   // 产品开通申请
   CPKT: {
-    // 初审状态（apply）
-    'C002': {
-      auditPath: 'businessFirstAudit',
-      detailPath: 'businessFirstAuditDetail'
-    },
-    // 复审状态
-    'C003': {
-      auditPath: 'businessReviewAudit',
-      detailPath: 'businessReviewAuditDetail'
-    },
-    // 复核驳回状态
-    'c006': {
-      auditPath: 'businessFirstBackAudit',
-      detailPath: 'businessFirstBackAuditDetail'
-    },
-    // 已办和办结状态
-    doneDetailPath: '',
+    // 产品申请
+    'CPSQ': {
+      // 初审状态（apply）
+      'C002': {
+        auditPath: 'businessFirstAudit',
+        detailPath: 'businessFirstAuditDetail'
+      },
+      // 复审状态
+      'C003': {
+        auditPath: 'businessReviewAudit',
+        detailPath: 'businessReviewAuditDetail'
+      },
+      // 复核驳回状态
+      'c006': {
+        auditPath: 'businessFirstBackAudit',
+        detailPath: 'businessFirstBackAuditDetail'
+      },
+      // 已办和办结状态
+      doneDetailPath: 'businessFirstAuditDetail',
+    }
   },
-
   // 业务管理
   HTXQ: {
-    auditPath: '',
-    detailPath: ''
+// 业务管理-合同续签
+    HTXQ: {
+      // 复审状态
+      'C003': {
+        auditPath: 'contractReSignReviewAudit',
+        detailPath: 'contractReSignReviewAuditDetail'
+      },
+      // 复核驳回状态
+      'c006': {
+        auditPath: 'contractReSignBackAudit',
+        detailPath: 'contractReSignBackAuditDetail'
+      },
+      // 已办和办结状态
+      doneDetailPath: 'contractReSignReviewAuditDetail',
+    },
+    // 业务管理-业务参数维护
+    YWCSWH: {
+      // 复审状态
+      'C003': {
+        auditPath: 'tradeChangeReviewAudit',
+        detailPath: 'tradeChangeReviewAuditDetail'
+      },
+      // 复核驳回状态
+      'c006': {
+        auditPath: 'tradeChangeBackAudit',
+        detailPath: 'tradeChangeBackAuditDetail'
+      },
+      // 已办和办结状态
+      doneDetailPath: 'tradeChangeReviewAuditDetail',
+    },
+    // 业务管理-额度管理
+    EDGL: {
+      // 复审状态
+      'C003': {
+        auditPath: 'quotaChangeReviewAudit',
+        detailPath: 'quotaChangeReviewAuditDetail'
+      },
+      // 复核驳回状态
+      'c006': {
+        auditPath: 'quotaChangeBackAudit',
+        detailPath: 'quotaChangeBackAuditDetail'
+      },
+      // 已办和办结状态
+      doneDetailPath: 'quotaChangeReviewAuditDetail',
+    }
   },
-
   // 确认收款
   QRSK: {
-    auditPath: '',
-    detailPath: ''
+    // 凭证收款
+    '00': {
+      // 待保理公司复审
+      'A002': {
+        auditPath: 'billConfirmPaymentAudit',
+        detailPath: 'billConfirmPaymentDetail'
+      },
+      'A003': {
+        auditPath: 'quotaChangeBackAudit',
+        detailPath: 'quotaChangeBackAuditDetail'
+      },
+      // 已办和办结状态
+      doneDetailPath: 'quotaChangeReviewAuditDetail',
+    },
+    // 非凭证收款
+    '01': {
+      'A002': {
+        auditPath: 'quotaChangeBackAudit',
+        detailPath: 'quotaChangeBackAuditDetail'
+      },
+      'A003': {
+        auditPath: 'quotaChangeBackAudit',
+        detailPath: 'quotaChangeBackAuditDetail'
+      },
+      // 已办和办结状态
+      doneDetailPath: 'quotaChangeReviewAuditDetail',
+    }
   },
 
   // 合同管理
