a91063d367570097a31d0da69397d052703e86d9
楚馥远 <chufuyuan@izejin.com>
2022-08-29 18:00:21 +0800

    凭证暴力融资台账联调

--- src/modules/ebill/views/queryStatistics/voucherFinancingAccount/detail.vue
+++ src/modules/ebill/views/queryStatistics/voucherFinancingAccount/detail.vue
@@ -2,7 +2,7 @@
   <zj-content-container>
     <!--  融资交易详情  -->
     <zj-top-header title="凭证融资详情" />
-    <zj-content-block v-if="workflow === 'sqxx'">
+    <zj-content-block v-show="workflow === 'sqxx'">
       <el-form :model="form" ref="form" label-width="200px" class="financingForm">
         <zj-content-block>
           <zj-header title="融资信息" />
@@ -104,26 +104,26 @@
       </el-form>
     </zj-content-block>
 
-    <zj-content-block v-if="workflow === 'pzxx' && row.financingProductType !== '0'">
+    <zj-content-block v-show="workflow === 'pzxx' && row.financingProductType !== '0'">
       <zj-content-block>
         <zj-header title="凭证信息" />
-        <zj-table ref="billInfoTable" class="zj-search-table" :dataList="voucherInfoList"
-          :params="{ id: row.writerId, serialNo: row.serialNo, }" @after-load="handleDataChange"
+        <zj-table ref="billtable" class="zj-search-table" :dataList="voucherInfoList" 
+          :params="{ id: row.writerId, serialNo: row.serialNo, }" 
           @radio-change="handleRadioChange" :radio-config="{ highlight: true }" :pager="false">
           <!-- :api="zjControl.getFinancingBillInfos" -->
           <zj-table-column type="radio" width="40" />
-          <zj-table-column field="ebillCode" title="凭证编号" />
-          <zj-table-column field="rootCode" title="原始凭证编号" />
+          <zj-table-column field="ebillCode" title="海e单编号" />
+          <zj-table-column field="rootCode" title="原始海e单编号" />
           <zj-table-column field="payEntName" title="凭证签发人" />
           <zj-table-column field="transferName" title="转让企业" />
           <zj-table-column field="payableIssuanceDate" title="签发日期" :formatter="date" />
-          <zj-table-column field="payableAmt" title="凭证金额" :formatter="money" />
-          <zj-table-column field="payableExpireDate" title="凭证到期日" :formatter="date" />
+          <zj-table-column field="payableAmt" title="海e单金额" :formatter="money" />
+          <zj-table-column field="payableExpireDate" title="海e单到期日" :formatter="date" />
         </zj-table>
       </zj-content-block>
       <zj-content-block>
-        <zj-header :title="`对账单信息${ebillCode ? `-${ebillCode}` : ''}`" />
-        <zj-table ref="searchTable" :dataList="ebillInfo.accountBillInners" :pager="false">
+        <zj-header :title="`对账单信息:（海e单编号：${this.bjcode})`" />
+        <zj-table ref="searchTable" :dataList="billList" :pager="false">
           <zj-table-column field="acctBillCode" title="对账单编号" />
           <zj-table-column field="companyName" title="买方名称" />
           <zj-table-column field="supplierCode" title="供应商业务系统编码" />
@@ -138,63 +138,29 @@
         </zj-table>
       </zj-content-block>
       <zj-content-block>
-        <zj-header :title="`贸易背景资料（资产编号：${this.activeEbillCodeNo}`" />
+        <zj-header :title="`贸易背景资料（资产编号：${this.bjcode})`" />
         <el-tabs v-model="tabs" class="zj-tabs-card">
           <el-tab-pane label="贸易合同信息" name="tradeContract">
-            <!-- <trade-contract :zjControl="zjControl" :form="ebillInfo.contractInfo" /> -->
-            <el-form :model="form" label-width="200px">
-              <el-row>
-                <el-col :span="8">
-                  <el-form-item label="贸易合同编号：">{{ BusinessContract.contractNo }}</el-form-item>
-                </el-col>
-                <el-col :span="8">
-                  <el-form-item label="买方：">{{ BusinessContract.buyerSigner }}</el-form-item>
-                </el-col>
-                <el-col :span="8">
-                  <el-form-item label="卖方：">{{ BusinessContract.sellerSigner }}</el-form-item>
-                </el-col>
-              </el-row>
-              <el-row>
-                <el-col :span="8">
-                  <el-form-item label="合同期限（月）：">{{ BusinessContract.contractMonthLimit }}</el-form-item>
-                </el-col>
-                <el-col :span="8">
-                  <el-form-item label="合同金额：">{{ BusinessContract.contractAmt }}</el-form-item>
-                </el-col>
-                <el-col :span="8">
-                  <el-form-item label="签订日期：">{{ BusinessContract.signDate }}</el-form-item>
-                </el-col>
-              </el-row>
-              <el-row>
-                <el-col :span="8">
-                  <el-form-item label="货物名称：">
-                    {{ BusinessContract.goodsName }}
-                  </el-form-item>
-                </el-col>
-                <el-col :span="8">
-                  <el-form-item label="合同附件：">
-                    <div class="atta-name-box">
-                      <span class="atta-name">
-                        {{ BusinessContract.fileName }}
-                      </span>
-                      <zj-button type="text" @click="attaDownload">下载</zj-button>
-                    </div>
-                  </el-form-item>
-                </el-col>
-              </el-row>
-              <el-row>
-                <el-col :span="8">
-                  <el-form-item label="阶段性协议编号：">{{ BusinessContract.agreementNo }}</el-form-item>
-                </el-col>
-                <el-col :span="8">
-                  <el-form-item label="阶段性协议名称：">{{ BusinessContract.agreementName }}</el-form-item>
-                </el-col>
-              </el-row>
-            </el-form>
+            <zj-table ref="searchTable" :dataList="BusinessContract">
+              <zj-table-column field="contractNo" title="贸易合同编号：" />
+              <zj-table-column field="buyerSigner" title="买方：" />
+              <zj-table-column field="sellerSigner" title="卖方：" />
+              <zj-table-column field="contractMonthLimit" title="合同期限（月）：" />
+              <zj-table-column field="contractAmt" title="合同金额：" />
+              <zj-table-column field="signDate" title="签订日期：" />
+              <zj-table-column field="goodsName	" title="货物名称：" :formatter="date" />
+              <zj-table-column field="fileName" title="合同附件：" />
+              <zj-table-column field="agreementNo" title="阶段性协议编号：" />
+              <zj-table-column field="agreementName" title="阶段性协议名称：" />
+              <zj-table-column title="操作" fixed="right">
+                <template v-slot="{ row }">
+                  <zj-button type="text" @click="downs(row)">下载</zj-button>
+                </template>
+              </zj-table-column>
+            </zj-table>
           </el-tab-pane>
           <el-tab-pane label="发票信息" name="invoice">
-            <!-- <invoice :invoiceList="ebillInfo.invoices" /> -->
-            <zj-table ref="searchTable" :dataList="invoiceList">
+            <zj-table ref="searchTable" :dataList="fplist">
               <zj-table-column field="invoiceType" title="发票类型" />
               <zj-table-column field="invoiceNumber" title="发票号码" />
               <zj-table-column field="invoiceCode" title="发票代码" />
@@ -212,7 +178,7 @@
           </el-tab-pane>
           <el-tab-pane label="其他附件" name="attaList">
             <!-- <attaList :attaList="ebillInfo.otherAttachs" /> -->
-            <zj-table ref="searchTable" :dataList="attaList">
+            <zj-table ref="searchTable" :dataList="businessOtherAttachList">
               <zj-table-column type="seq" title="序号" />
               <zj-table-column field="fileName" title="文件名称" />
               <zj-table-column title="操作" fixed="right">
@@ -244,24 +210,17 @@
 </template>
 
 <script>
-import operateLog from "@modules/workflow/views/components/operateLog";
-// import tradeContract from '../components/tradeContract'
-// import invoice from '../components/invoice'
-// import attaList from '../components/attaList'
 
 
 export default {
   name: "toDoDetail",
   components: {
-    // tradeContract,
-    // invoice,
-    // attaList,
-    // operateLog,
   },
   watch: {
-    // workflow() {
-    //   this.getDetail()
-    // }
+    workflow() {
+      // this.setFirstRow(this.voucherInfoList[0]);
+      this.getOtherDetail()
+    }
   },
   data() {
     return {
@@ -290,18 +249,42 @@ export default {
       rzxylist: [],//融资协议列表
       backmoney: [],//还款记录列表
       otherfilelist: [],//其他协议列表
-      xqx:{},//申请信息详情
-      voucherInfoList:{},//凭证信息列表
-      BusinessContract:{},//贸易合同信息（接口返回的外层是数组，记得拆）
+      xqx: {},//申请信息详情
+      voucherInfoList: {},//凭证信息列表
+      BusinessContract: [],//贸易合同信息
+      billList: [{//对账单信息列表
+        acctBillCode: "",
+        companyName: "",
+        supplierCode: "",
+        supplierName: "",
+        checkBillDate: "",
+        inputDate: "",
+        estimatedPaymentDate: "",
+        checkBillAmt: "",
+        isIssueVoucher: "",
+        billSource: ""
+      }],
+      bjcode: "",//贸易背景资料编号
+      fplist: [],//发票信息
+      businessOtherAttachList:[],//其他附件
     }
   },
   created() {
     this.getApi()
     this.getRow()
     // this.getDictionary()
+    console.log("created");
+    // this.$refs.billInfoTable
+  },
+  mounted(){
+    console.log("mounted");
     this.getDetail()
   },
   methods: {
+    // setFirstRow(row) {
+    //   this.$refs.billTable.setRadioRow(row);
+    //   this.handleRadioChange({row});
+    // },
     getDictionary() {
       this.zjControl.getFinancingTransDirectory().then(res => {
         this.dictionary = Object.assign({}, res.data)
@@ -314,57 +297,99 @@ export default {
       console.log(params);
       //申请信息
       this.zjControl.rdDetailrzxq(params).then(res => {
-          this.xqx = res.data
-          this.rzxylist = res.data.agreementFileList
-          this.backmoney = res.data.repaymentRecordList
-          this.otherfilelist = res.data.otherAttachList
-        })
+        this.xqx = res.data
+        this.rzxylist = res.data.agreementFileList
+        this.backmoney = res.data.repaymentRecordList
+        this.otherfilelist = res.data.otherAttachList
+      })
       // //凭证信息
       this.zjControl.rdDetailpzxx(params).then(res => {
-        this.voucherInfoList=res.data.voucherInfoList
+        this.voucherInfoList = res.data.voucherInfoList
+        console.log("默认首选",this.$refs);
+        this.$refs.billtable.setRadioRow(this.voucherInfoList[0]);
+        this.handleRadioChange(this.voucherInfoList[0]);
       })
+        
+    },
+    getOtherDetail(){
       if (this.workflow === 'sqxx') {
         console.log("申请信息时触发");
       }
       if (this.workflow === 'pzxx') {
         console.log("凭证信息时触发");
+        let params = {
+        ebillCode: this.voucherInfoList[0].ebillCode,
+        serialNo: this.row.serialNo
+      }
+      this.zjControl.getOtherInfoByBill(params).then(res => {
+        //对账单列表
+        this.billList[0].acctBillCode = res.data.acctBillCode
+        this.billList[0].companyName = res.data.companyName
+        this.billList[0].supplierCode = res.data.supplierCode
+        this.billList[0].supplierName = res.data.supplierName
+        this.billList[0].checkBillDate = res.data.checkBillDate
+        this.billList[0].inputDate = res.data.inputDate
+        this.billList[0].estimatedPaymentDate = res.data.estimatedPaymentDate
+        this.billList[0].checkBillAmt = res.data.checkBillAmt
+        this.billList[0].isIssueVoucher = res.data.isIssueVoucher
+        this.billList[0].billSource = res.data.billSource
+        //贸易背景资料
+        this.bjcode = res.data.ebillCode
+        //发票信息
+        this.fplist = res.data.businessInvoiceList
+        // 贸易合同信息
+        this.BusinessContract = res.data.businessContractList
+        // 其他附件
+        this.businessOtherAttachList=res.data.businessOtherAttachList
+      })
+
       }
     },
     agreementDownLoad() {
 
     },
-    attaDownLoad() { 
+    downs() {
       console.log("下载还没做");
     },
 
     handleDataChange(rows) {
+      console.log("默认勾选第一个凭证信息");
       //默认勾选第一个凭证信息
       if (rows && rows.length) {
-        this.$refs.billInfoTable.setRadioRow(rows[0])
-        this.handleRadioChange({ row: rows[0] })
+        // this.$refs.billTable.setRadioRow(rows[0])
+        // this.handleRadioChange({ row: rows[0] })
       }
     },
     handleRadioChange({ row }) {
-      
+
       let params = {
-        ebillCode:row.ebillCode,
-        serialNo:this.row.serialNo
+        ebillCode: row.ebillCode,
+        serialNo: this.row.serialNo
       }
       this.zjControl.getOtherInfoByBill(params).then(res => {
         //对账单列表
-        this.ebillInfo.accountBillInners = res.data.accountBillInners
-        //对账单编号
-        this.activeEbillCodeNo = res.data.acctBillCode
-        //背景资料信息列表
-        this.ebillInfo.contractInfo = res.data.contractInfos[0]
-        this.ebillInfo.invoices = res.data.invoices
-        this.ebillInfo.otherAttachs = res.data.otherAttachs
+        this.billList[0].acctBillCode = res.data.acctBillCode
+        this.billList[0].companyName = res.data.companyName
+        this.billList[0].supplierCode = res.data.supplierCode
+        this.billList[0].supplierName = res.data.supplierName
+        this.billList[0].checkBillDate = res.data.checkBillDate
+        this.billList[0].inputDate = res.data.inputDate
+        this.billList[0].estimatedPaymentDate = res.data.estimatedPaymentDate
+        this.billList[0].checkBillAmt = res.data.checkBillAmt
+        this.billList[0].isIssueVoucher = res.data.isIssueVoucher
+        this.billList[0].billSource = res.data.billSource
+        //贸易背景资料
+        this.bjcode = res.data.ebillCode
+        //发票信息
+        this.fplist = res.data.businessInvoiceList
         // 贸易合同信息
-        this.BusinessContract=res.data.businessContractList[0]
+        this.BusinessContract = res.data.businessContractList
+        // 其他附件
+        this.businessOtherAttachList=res.data.businessOtherAttachList
       })
     },
   },
-  
+
 }
 </script>
 
