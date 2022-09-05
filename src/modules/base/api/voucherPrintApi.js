5b4ee4260a16835c97fba28e0138197a243bc0ef
lijl17 <lijl17@haday.cn>
2022-09-05 15:35:45 +0800

    增加凭据打印功能

--- src/modules/base/api/voucherPrintApi.js
+++ src/modules/base/api/voucherPrintApi.js
@@ -5,20 +5,20 @@ import request from '@common/axios/request'
 
 const voucherPrint = {
     //单笔凭证打印-数据字典
-    getRolePower: (params) => {
-        return request.get('/voucher-print/get-single-voucher-directory', { params })
+    getSingleVoucherDirectory: (params) => {
+        return request.get('/more-voucher-print/get-more-voucher-directory', { params })
     },
     //凭证打印-打印凭证
-    queryRolePage: (params) => {
-        return request.get('/voucher-print/get-voucher-print', { params })
+    getVoucherPrint: (params) => {
+        return request.get('/more-voucher-print/get-more-voucher-print', { params })
     },
     //凭据打印-查询
-    updateRolePower: (params) => {
-        return request.get('/voucher-print/query-voucher-list', { params })
+    queryVoucherList: (params) => {
+        return request.get('/more-voucher-print/query-more-voucher-list', { params })
     },
     //凭证打印-更新打印次数
-    addRole: (params) => {
-        return request.post('/voucher-print/update-voucher-print-times', params)
+    updateVoucherPrintTimes: (params) => {
+        return request.post('/more-voucher-print/update-more-voucher-print-num', params)
     },
 }
 
