17afa391c6a3893f32e1a3b58c86272330ccc611
吕凤文 <wlvfw@fsht.com>
2022-09-07 13:42:12 +0800

    修改logo和名称

--- src/modules/base/views/sysModules/register/register/components/RegisterHeader.vue
+++ src/modules/base/views/sysModules/register/register/components/RegisterHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="LoginHeader">
-      <div class="logo" @click="toLogin"><a href="#"><img src="@assets/img/layout/Logo.png"/></a></div>
+      <div class="logo" @click="toLogin"><a href="#"><img src="@assets/img/layout/Logo2.png"/></a></div>
       <ul class="nav" :class="close ? 'zj-m-r-20' : 'zj-m-r-010'"> <!-- :style="{marginRight: close ? '20px' : '10%'}" -->
         <li v-for="(item,index) in routerList" :key="index">
           <router-link :to="item.path">{{item.label}}</router-link>
@@ -75,8 +75,8 @@
         height: 52px;
         img{
             //border-top: 5px solid rgba(0,0,0,0);
-            width: 120px;
-            height: 70px;
+            width: 60px;
+            height: 60px;
         }
     }
     .nav{
