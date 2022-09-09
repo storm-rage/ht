84cd1c24ea4fb058dc4f6754f9d490c93737f9e7
wangqiang <13169658096@123.com>
2022-09-09 17:44:23 +0800

    企业修改

--- src/modules/base/views/entUserManage/userManage/userAdd.vue
+++ src/modules/base/views/entUserManage/userManage/userAdd.vue
@@ -38,6 +38,12 @@
         <zj-header title="用户信息"></zj-header>
         <zj-content>
           <el-row>
+            <el-col :span="24">
+              <el-form-item label="海天业务系统账号：" prop="htSysCode">
+                <el-input v-model="form.htSysCode" />
+                <zj-button class="zj-m-l-20" type="primary" @click="getEmployeeInfo">确认</zj-button>
+              </el-form-item>
+            </el-col>
             <el-col :span="8">
               <el-form-item label="用户姓名：" prop="userName">
                 <el-input v-model="form.userName" />
@@ -71,34 +77,26 @@
                 <el-input v-model="form.email" />
               </el-form-item>
             </el-col>
-            <el-col :span="8">
-              <el-form-item label="海天业务系统账号：" prop="isHtSysCode">
-                <el-input v-model="form.htSysCode" />
+          </el-row>
+          <el-row>
+            <el-col :span="24">
+              <el-form-item label="用户角色：">
+                <el-select class="rolesSelect" v-model="form.roleIds" filterable placeholder="请选择" :popper-append-to-body="false" multiple @change="roleChange">
+                  <el-option v-for="item in dictionary.autoRoleList" :key="item.code" :label="item.desc" :value="item.code" :disabled="item.disabled">
+                  </el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="24">
+              <el-form-item label="请选择开凭证对账单类型权限：" label-width="280px" v-if="isShowType">
+                <el-select v-model="statementAccountTypeArr" filterable placeholder="请选择" :popper-append-to-body="false" multiple>
+                  <el-option v-for="item in dictionary.statementAccountTypeList" :key="item.code" :label="item.desc" :value="item.code">
+                  </el-option>
+                </el-select>
               </el-form-item>
             </el-col>
           </el-row>
-          <el-row v-for="(item, index) in form.roles" :key="item.key">
-            <zj-content>
-              <el-col>
-                <el-form-item label="用户角色：">
-                  <el-select v-model="form.roles[index].roleId" filterable placeholder="请选择" :popper-append-to-body="false" multipl @change="roleChange">
-                    <el-option v-for="item in dictionary.autoRoleList" :key="item.code" :label="item.desc" :value="item.code" :disabled="item.disabled">
-                    </el-option>
-                  </el-select>
-                  <el-button @click="addDomRole" class="zj-m-l-20" v-if="form.roles.length < 2">新增</el-button>
-                  <el-button @click="deleteDomRole(index)" class="zj-m-l-20" v-if="index>0">删除</el-button>
-                </el-form-item>
-                <el-form-item label="请选择开凭证对账单类型权限：" label-width="280px" v-if="showStatementAccountType(form.roles[index].roleId)">
-                  <el-select v-model="form.roles[index].statementAccountTypeArr" filterable placeholder="请选择" :popper-append-to-body="false" multiple>
-                    <el-option v-for="item in dictionary.statementAccountTypeList" :key="item.code" :label="item.desc" :value="item.code">
-                    </el-option>
-                  </el-select>
-                </el-form-item>
-              </el-col>
-            </zj-content>
-
-          </el-row>
-          <zj-collapse title="身份证">
+          <zj-collapse title="身份证" style="margin-top: 80px">
             <zj-table :pager="false" ref="attach" :dataList="attachInfo">
               <zj-table-column type="seq" width="60" title="序号" />
               <zj-table-column field="type" title="附件类型" />
@@ -131,16 +129,15 @@ export default {
   components: {},
   data() {
     return {
-      zjControl: this.$api.userInfoManage,
-      form: {
-        roles: [
-          {
-            roleId: '',
-            statementAccountType: '',
-            key: Date.now()
-          }
-        ],
+      zjControl: {
+        ...this.$api.userInfoManage,
+        getEmployeeInfo: this.$api.entInfoManage.getEmployeeInfo
       },
+      form: {
+        certType: '01',
+        roleIds: [],
+      },
+      statementAccountTypeArr: [],
       detailData: {},
       dictionary: {},
       attachInfo: [{ fileId: "", type: "身份证影印件", fileName: "" }],
@@ -159,7 +156,7 @@ export default {
             trigger: ["blur"],
           },
         ],
-        isHtSysCode: [
+        htSysCode: [
           {
             required: true,
             message: "海天业务系统账号",
@@ -190,34 +187,31 @@ export default {
           },
         ],
       },
-      rolesActive: []
+      rolesActive: [],
+      isShowType: true,
     };
   },
   created() {
     this.getRow();
     this.getDictionary();
   },
+  watch: {
+    // 选择经办，复核显示对账单类型
+    // 'form.roleIds'(roles) {
+    //   console.log(roles)
+    //   let codeArr = ['5', '6', '8', '9', '11', '12', '14', '15']
+    //   this.isShowType = false
+    //   roles.forEach(item => {
+    //     if (codeArr.includes(item)) {
+    //       console.log(this.isShowType)
+    //       return this.isShowType = true
+    //     }
+    //   })
+    // }
+  },
   methods: {
-    // 选择经办显示对账单类型
-    showStatementAccountType(code) {
-      let codeArr = ["5", "8", "11", "14"]
-      if (codeArr.includes(code)) {
-        return true
-      } else {
-        return false
-      }
-    },
-    roleChange(val) {
-      // this.form.roles.forEach(item => {
-      //   this.rolesActive.push(item.roleId)
-      // })
-      // console.log(this.rolesActive)
-      // this.dictionary.autoRoleList.forEach(item => {
-      //   if (item.code === val) {
-      //     item.disabled = true
-      //   }
-      // });
-      // console.log(this.dictionary.autoRoleList)
+    roleChange(roles) {
+
     },
     //获取字典
     getDictionary() {
@@ -249,6 +243,41 @@ export default {
           console.log(this.dictionary)
         });
     },
+    // 获取海天员工信息
+    getEmployeeInfo() {
+      this.zjControl.getEmployeeInfo({ htSysCode: this.form.htSysCode, customCode: this.detailData.customCode }).then((res) => {
+        if (res.data.realName) {
+          let sysUserItem = {
+            userName: res.data.realName,//用户名
+            certNo: res.data.idcardNo,//证件号码
+            mobileNo: res.data.tel,//手机号码
+            email: res.data.email,//电子邮箱
+            // roleIds: [],//操作员角色
+            // statementAccountType: [], //对账单类型
+          }
+          this.form.roleIds = res.data.roleIds
+          if (res.data.statementAccountType.split(',')) {
+            let statementAccountTypeArr = res.data.statementAccountType.split(',')
+            statementAccountTypeArr.forEach((item, index) => {
+              this.$set(this.statementAccountTypeArr, index, item)
+            })
+          }
+          // this.statementAccountTypeArr = res.data.statementAccountType
+          // 返回的数据不能修改
+          if (!!sysUserItem.htSysCode) sysUserItem.isHtSysCodeDis = true //是否禁用
+          if (!!sysUserItem.userName) sysUserItem.isUserNameDis = true //是否禁用
+          if (!!sysUserItem.certNo) sysUserItem.isCertNoDis = true //是否禁用
+          if (!!sysUserItem.mobileNo) sysUserItem.isMobileNoNameDis = true //是否禁用
+          if (!!sysUserItem.email) sysUserItem.isEmailDis = true //是否禁用
+          this.form = Object.assign(this.form, sysUserItem)
+          console.log(this.form)
+
+          this.$message.success('查询成功！')
+        } else {
+          this.$message.error('娅米账号/业务系统账号不存在！')
+        }
+      })
+    },
     //上传附件
     handleFileUpload(data) {
       let formData = new FormData();
@@ -262,30 +291,19 @@ export default {
     // 新增用户
     submit() {
       let params = Object.assign(this.form, this.detailData);
-      params.roles.forEach(item => {
-        if (item.statementAccountTypeArr) {
-          item.statementAccountType = item.statementAccountTypeArr.join(',')
-        }
-        delete item.statementAccountTypeArr
-      })
+      // if (roleArr.includes('5') && roleArr.includes('6') || roleArr.includes('8') && roleArr.includes('9') || roleArr.includes('11') && roleArr.includes('12')||roleArr.includes('14') && roleArr.includes('15')) {
+      //   this.$message.error('同一账号不能同时选经办员和复核员！')
+      //   return
+      // }
+      if (this.statementAccountTypeArr) {
+        params.statementAccountType = this.statementAccountTypeArr.join(',')
+      }
       params.idCardAttach = this.attachInfo // 身份证附件
       this.zjControl.addUser(params).then((res) => {
         this.$message.success("新增用户成功！");
         this.goParent();
       });
     },
-    // 新增角色
-    addDomRole() {
-      this.form.roles.push({
-        roleId: '',
-        statementAccountType: '',
-        key: Date.now()
-      });
-    },
-    // 删除新增角色
-    deleteDomRole(index) {
-      this.form.roles.splice(index, 1)
-    }
   },
 };
 </script>
@@ -309,6 +327,13 @@ export default {
     .el-form-item__error {
       top: 14px;
     }
+  }
+}
+
+.rolesSelect {
+  width: 400px;
+  /deep/.el-input {
+    width: 400px;
   }
 }
 </style>
